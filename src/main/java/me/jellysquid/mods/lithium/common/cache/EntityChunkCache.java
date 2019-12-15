1a0d59f96c9f5f8817dd210ab1cf89d9a969f238
Angeline <jellysquid3@users.noreply.github.com>
2019-12-14 19:31:35 -0600

    Ensure the entity chunk cache is purged on entity teleportation

--- src/main/java/me/jellysquid/mods/lithium/common/cache/EntityChunkCache.java
+++ src/main/java/me/jellysquid/mods/lithium/common/cache/EntityChunkCache.java
@@ -4,6 +4,7 @@ import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.Entity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.util.math.BlockPos;
@@ -12,6 +13,7 @@ import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkManager;
 import net.minecraft.world.chunk.ChunkSection;
 import net.minecraft.world.chunk.WorldChunk;
 
@@ -19,19 +21,23 @@ import net.minecraft.world.chunk.WorldChunk;
  * Maintains a cached collection of chunks around an entity. This allows for much faster access to nearby chunks for
  * many entity related functions.
  */
-@SuppressWarnings("ForLoopReplaceableByForEach")
 public class EntityChunkCache extends AbstractCachedAccess {
     private final Long2ObjectMap<WorldChunk> chunks = new Long2ObjectOpenHashMap<>(9);
 
     @SuppressWarnings("unchecked")
     private final CachedEntry<WorldChunk>[] mra = new CachedEntry[3];
 
-    private final World world;
+    private final Entity entity;
+
+    private ChunkManager chunkManager;
+    private World world;
 
     private int minX, minZ, maxX, maxZ;
 
-    public EntityChunkCache(World world) {
-        this.world = world;
+    public EntityChunkCache(Entity entity) {
+        this.entity = entity;
+        this.world = entity.getEntityWorld();
+        this.chunkManager = entity.getEntityWorld().getChunkManager();
 
         for (int i = 0; i < this.mra.length; i++) {
             this.mra[i] = new CachedEntry<>();
@@ -43,30 +49,30 @@ public class EntityChunkCache extends AbstractCachedAccess {
     }
 
     public void updateChunks(Box box) {
-        int blockMinX = MathHelper.floor(box.minX - 16.0D);
-        int blockMaxX = MathHelper.ceil(box.maxX + 16.0D);
-        int blockMinZ = MathHelper.floor(box.minZ - 16.0D);
-        int blockMaxZ = MathHelper.ceil(box.maxZ + 16.0D);
-
-        int minX = blockMinX >> 4;
-        int maxX = blockMaxX >> 4;
-        int minZ = blockMinZ >> 4;
-        int maxZ = blockMaxZ >> 4;
+        int minX = MathHelper.floor(box.minX - 16.0D) >> 4;
+        int maxX = MathHelper.ceil(box.maxX + 16.0D) >> 4;
+        int minZ = MathHelper.floor(box.minZ - 16.0D) >> 4;
+        int maxZ = MathHelper.ceil(box.maxZ + 16.0D) >> 4;
 
         // If we're not watching any new chunks, we have no need to update the set.
-        if (!this.chunks.isEmpty() && this.minX == minX && this.maxX == maxX && this.minZ == minZ && this.maxZ == maxZ) {
-            return;
+        if (!this.chunks.isEmpty() && this.entity.getEntityWorld().getChunkManager() == this.chunkManager) {
+            if (this.minX == minX && this.maxX == maxX && this.minZ == minZ && this.maxZ == maxZ) {
+                return;
+            }
         }
 
         this.chunks.clear();
 
-        for (int i = 0; i < this.mra.length; i++) {
-            this.mra[i].reset();
+        for (CachedEntry<WorldChunk> worldChunkCachedEntry : this.mra) {
+            worldChunkCachedEntry.reset();
         }
+
+        this.world = this.entity.getEntityWorld();
+        this.chunkManager = this.world.getChunkManager();
 
         for (int x = minX; x < maxX; x++) {
             for (int z = minZ; z < maxZ; z++) {
-                WorldChunk chunk = this.world.getChunkManager().getWorldChunk(x, z, false);
+                WorldChunk chunk = this.chunkManager.getWorldChunk(x, z, false);
 
                 this.chunks.put(ChunkPos.toLong(x, z), chunk);
             }
@@ -86,7 +92,7 @@ public class EntityChunkCache extends AbstractCachedAccess {
         Chunk chunk = this.getWorldChunk(x, z);
 
         if (chunk == null) {
-            chunk = this.world.getChunk(x, z);
+            chunk = this.chunkManager.getWorldChunk(x, z, false);
         }
 
         if (chunk != null) {
@@ -129,9 +135,9 @@ public class EntityChunkCache extends AbstractCachedAccess {
         if (this.chunks.size() > 0) {
             long key = ChunkPos.toLong(x, z);
 
-            for (int i = 0; i < this.mra.length; i++) {
-                if (this.mra[i].pos == key) {
-                    return this.mra[i].obj;
+            for (CachedEntry<WorldChunk> worldChunkCachedEntry : this.mra) {
+                if (worldChunkCachedEntry.pos == key) {
+                    return worldChunkCachedEntry.obj;
                 }
             }
 
