aa601644735e18f1dc965bc321be93251367f776
Angeline <jellysquid3@users.noreply.github.com>
2020-01-28 12:53:00 -0600

    Bump to Minecraft 1.15.2 and Fabric Loader 0.7.6

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/chunk_cache/MixinLivingEntity.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/chunk_cache/MixinLivingEntity.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @SuppressWarnings("InvalidMemberReference")
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity {
-    @Redirect(method = {"baseTick", "onDeath", "method_23733", "isClimbing", "handleFallDamage", "getBlockState", "travel", "canEnterTrapdoor", "method_23328", "applyClimbingSpeed" },
+    @Redirect(method = {"baseTick", "onDeath", "isClimbing", "handleFallDamage", "getBlockState", "travel", "canEnterTrapdoor", "applyClimbingSpeed" },
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;"))
     private BlockState redirectGetBlockState(World world, BlockPos pos) {
         EntityChunkCache cache = EntityWithChunkCache.getChunkCache((Entity) (Object) this);
