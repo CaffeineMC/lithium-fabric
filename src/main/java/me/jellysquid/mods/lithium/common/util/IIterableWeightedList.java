83c33390c8bc040e0bca0144bc68744bb0e39798
Angeline <jellysquid3@users.noreply.github.com>
2020-03-02 01:55:45 -0600

    new: Optimize the CompositeTask type used by villagers
    Replaces yet more stream-based code with faster iterator logic

---
+++ src/main/java/me/jellysquid/mods/lithium/common/util/IIterableWeightedList.java
@@ -0,0 +1,32 @@
+package me.jellysquid.mods.lithium.common.util;
+
+import net.minecraft.util.WeightedList;
+
+import java.util.Iterator;
+
+public interface IIterableWeightedList<U> extends Iterable<U> {
+    Iterator<U> iterator();
+
+    @SuppressWarnings("unchecked")
+    static <T> Iterable<? extends T> cast(WeightedList<T> list) {
+        return ((IIterableWeightedList<T>) list);
+    }
+
+    class ListIterator<U> implements Iterator<U> {
+        private final Iterator<WeightedList<U>.Entry<? extends U>> inner;
+
+        public ListIterator(Iterator<WeightedList<U>.Entry<? extends U>> inner) {
+            this.inner = inner;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.inner.hasNext();
+        }
+
+        @Override
+        public U next() {
+            return this.inner.next().getElement();
+        }
+    }
+}
---
+++ src/main/java/me/jellysquid/mods/lithium/mixin/ai/fast_brain/MixinWeightedList.java
@@ -0,0 +1,22 @@
+package me.jellysquid.mods.lithium.mixin.ai.fast_brain;
+
+import me.jellysquid.mods.lithium.common.util.IIterableWeightedList;
+import net.minecraft.util.WeightedList;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Iterator;
+import java.util.List;
+
+@Mixin(WeightedList.class)
+public class MixinWeightedList<U> implements IIterableWeightedList<U> {
+    @Shadow
+    @Final
+    protected List<WeightedList<U>.Entry<? extends U>> entries;
+
+    @Override
+    public Iterator<U> iterator() {
+        return new IIterableWeightedList.ListIterator<>(this.entries.iterator());
+    }
+}
