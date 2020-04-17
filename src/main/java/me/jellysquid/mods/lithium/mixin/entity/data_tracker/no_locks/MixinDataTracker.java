3586aa387e91dfa6c25d53913b9c4d1b3747f2f3
Angeline <jellysquid3@users.noreply.github.com>
2020-04-16 19:21:32 -0500

    change: Replace lock implementation in DataTracker instead of redirecting usages

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/data_tracker/no_locks/MixinDataTracker.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/data_tracker/no_locks/MixinDataTracker.java
@@ -1,11 +1,17 @@
 package me.jellysquid.mods.lithium.mixin.entity.data_tracker.no_locks;
 
+import me.jellysquid.mods.lithium.common.util.lock.NullReadWriteLock;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.data.DataTracker;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
 
 /**
  * The vanilla implementation of {@link DataTracker} performs locking when fetching or updating data due to a legacy
@@ -18,17 +24,13 @@ import java.util.concurrent.locks.Lock;
  */
 @Mixin(value = DataTracker.class, priority = 1001)
 public abstract class MixinDataTracker {
-    @Redirect(method = {
-            "getEntry", "addTrackedData", "getDirtyEntries", "getAllEntries", "writeUpdatedEntries", "clearDirty"
-    }, at = @At(value = "INVOKE", target = "Ljava/util/concurrent/locks/Lock;lock()V"))
-    private void nullifyLock(Lock lock) {
+    @Mutable
+    @Shadow
+    @Final
+    private ReadWriteLock lock;
 
-    }
-
-    @Redirect(method = {
-            "getEntry", "addTrackedData", "getDirtyEntries", "getAllEntries", "writeUpdatedEntries", "clearDirty"
-    }, at = @At(value = "INVOKE", target = "Ljava/util/concurrent/locks/Lock;unlock()V"))
-    private void nullifyUnlock(Lock lock) {
-
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void init(Entity entity, CallbackInfo ci) {
+        this.lock = new NullReadWriteLock();
     }
 }
