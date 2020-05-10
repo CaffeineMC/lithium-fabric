f27252d29c4c88489c1fb5d9b1dcf79a9d54b21b
Angeline <jellysquid3@users.noreply.github.com>
2020-05-10 00:04:18 -0500

    refactor: Simplify the BlockState->PathNodeType cache in MixinLandPathNodeMaker

--- src/main/java/me/jellysquid/mods/lithium/mixin/ai/pathing/MixinLandPathNodeMaker.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/ai/pathing/MixinLandPathNodeMaker.java
@@ -1,7 +1,7 @@
 package me.jellysquid.mods.lithium.mixin.ai.pathing;
 
-import it.unimi.dsi.fastutil.objects.Reference2IntMap;
-import it.unimi.dsi.fastutil.objects.Reference2IntOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Reference2ReferenceMap;
+import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
 import net.minecraft.block.*;
 import net.minecraft.entity.ai.pathing.LandPathNodeMaker;
 import net.minecraft.entity.ai.pathing.NavigationType;
@@ -27,13 +27,8 @@ public abstract class MixinLandPathNodeMaker {
     private static final PathNodeType[] NODE_TYPES = PathNodeType.values();
 
     // This is not thread-safe!
-    private static final Reference2IntMap<BlockState> commonTypes = new Reference2IntOpenHashMap<>();
-    private static final Reference2IntMap<BlockState> neighborTypes = new Reference2IntOpenHashMap<>();
-
-    static {
-        commonTypes.defaultReturnValue(Integer.MIN_VALUE);
-        neighborTypes.defaultReturnValue(Integer.MIN_VALUE);
-    }
+    private static final Reference2ReferenceMap<BlockState, PathNodeType> commonTypes = new Reference2ReferenceOpenHashMap<>();
+    private static final Reference2ReferenceMap<BlockState, PathNodeType> neighborTypes = new Reference2ReferenceOpenHashMap<>();
 
     @Shadow
     private static boolean method_27138(BlockState blockState) {
@@ -48,43 +43,35 @@ public abstract class MixinLandPathNodeMaker {
     public static PathNodeType getCommonNodeType(BlockView blockView, BlockPos blockPos) {
         BlockState blockState = blockView.getBlockState(blockPos);
 
+        // Check early if the block is air as it will always be open regardless of other conditions
         if (blockState.isAir()) {
             return PathNodeType.OPEN;
         }
 
         // Get the cached type for this block state
-        int typeInt = commonTypes.getInt(blockState);
+        PathNodeType type = commonTypes.get(blockState);
 
-        // The computed type for this position in the world
-        PathNodeType type = null;
-
-        // If the cached value is positive, it refers to a valid cached type
-        if (typeInt > 0) {
-            return NODE_TYPES[typeInt];
+        // No result has been cached for this block state yet, so calculate and cache it
+        if (type == null) {
+            commonTypes.put(blockState, type = getTaggedBlockType$lithium(blockState));
         }
 
-        if (typeInt == Integer.MIN_VALUE) {
-            // No result has been cached for this block state yet
-            type = getTaggedBlockType$lithium(blockState);
+        // If the node type is open, it means that we were unable to determine a more specific type, so we need
+        // to check the fallback path.
+        if (type == PathNodeType.OPEN) {
+            // This is only ever called in vanilla after all other possibilities are exhausted, but before fluid checks
+            // It should be safe to perform it last in actuality and take advantage of the cache for fluid types as well
+            // since fluids will always pass this check.
+            if (!blockState.canPathfindThrough(blockView, blockPos, NavigationType.LAND)) {
+                return PathNodeType.BLOCKED;
+            }
 
-            // If the result is null for this block, then use a special value to signal it
-            commonTypes.put(blockState, type == null ? -1 : type.ordinal());
+            // All checks succeed, this path node really is open!
+            return PathNodeType.OPEN;
         }
 
-        // We found a type, so return it early now to emulate vanilla behavior
-        if (type != null) {
-            return type;
-        }
-
-        // We weren't able to decide a node type earlier, so perform the fallback path
-        // This is only ever called in vanilla after all other possibilities are exhausted, but before fluid checks
-        // It should be safe to perform it last in actuality and take advantage of the cache for fluid types as well
-        // since fluids will always pass this check.
-        if (!blockState.canPathfindThrough(blockView, blockPos, NavigationType.LAND)) {
-            return PathNodeType.BLOCKED;
-        }
-
-        return PathNodeType.OPEN;
+        // Return the cached value since we found an obstacle earlier
+        return type;
     }
 
     // [VanillaCopy] LandPathNodeMaker#getCommonNodeType
@@ -150,7 +137,7 @@ public abstract class MixinLandPathNodeMaker {
             return PathNodeType.LAVA;
         }
 
-        return null;
+        return PathNodeType.OPEN;
     }
 
     /**
@@ -171,15 +158,13 @@ public abstract class MixinLandPathNodeMaker {
 
                         BlockState state = blockView.getBlockState(pos);
 
-                        // Check if the block is air first to avoid expensive map lookups
-                        if (state.isAir()) {
-                            continue;
-                        }
+                        // Ensure that the block isn't air first to avoid expensive map lookups
+                        if (!state.isAir()) {
+                            PathNodeType neighborType = getNodeTypeForNeighbor(state);
 
-                        PathNodeType neighborType = getNodeTypeForNeighbor(state);
-
-                        if (neighborType != null) {
-                            type = neighborType;
+                            if (neighborType != PathNodeType.OPEN) {
+                                type = neighborType;
+                            }
                         }
                     }
                 }
@@ -190,35 +175,27 @@ public abstract class MixinLandPathNodeMaker {
     }
 
     private static PathNodeType getNodeTypeForNeighbor(BlockState state) {
-        PathNodeType type;
+        PathNodeType type = neighborTypes.get(state);
 
-        int typeInt = neighborTypes.getInt(state);
-
-        // If the cached value is positive, it refers to a valid cached type
-        if (typeInt >= 0) {
-            type = NODE_TYPES[typeInt];
-        } else if (typeInt == Integer.MIN_VALUE) {
-            // There is no cached value yet, so we need to compute it
-
-            // [VanillaCopy] LandPathNodeMaker#getNodeTypeFromNeighbors
-            // Determine what kind of obstacle type this neighbor is
-            if (state.isOf(Blocks.CACTUS)) {
-                type = PathNodeType.DANGER_CACTUS;
-            } else if (state.isOf(Blocks.SWEET_BERRY_BUSH)) {
-                type = PathNodeType.DANGER_OTHER;
-            } else if (method_27138(state)) {
-                type = PathNodeType.DANGER_FIRE;
-            } else {
-                type = null;
-            }
-
-            // If no obstacle is provided by this block, then use a special value to signal it
-            neighborTypes.put(state, type == null ? -1 : type.ordinal());
-        } else {
-            // The cached value is negative and not absent, so we already computed this result earlier and
-            // there was no obstacle
-            type = null;
+        // We already cached a type for this block state, so return it
+        if (type != null) {
+            return type;
         }
+
+        // [VanillaCopy] LandPathNodeMaker#getNodeTypeFromNeighbors
+        // Determine what kind of obstacle type this neighbor is
+        if (state.isOf(Blocks.CACTUS)) {
+            type = PathNodeType.DANGER_CACTUS;
+        } else if (state.isOf(Blocks.SWEET_BERRY_BUSH)) {
+            type = PathNodeType.DANGER_OTHER;
+        } else if (method_27138(state)) {
+            type = PathNodeType.DANGER_FIRE;
+        } else {
+            type = PathNodeType.OPEN;
+        }
+
+        // If no obstacle is provided by this block, then use a special value to signal it
+        neighborTypes.put(state, type);
 
         return type;
     }
