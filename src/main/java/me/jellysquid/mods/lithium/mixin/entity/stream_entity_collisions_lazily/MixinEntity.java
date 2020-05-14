e093595eb76eec4350cc8283c62ec4f7e78fccd3
2No2Name <2No2Name@web.de>
2020-05-14 03:33:28 +0200

    Remove redundant lazy evaluated VoxelShape stream Supplier stream.

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/stream_entity_collisions_lazily/MixinEntity.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/stream_entity_collisions_lazily/MixinEntity.java
@@ -15,22 +15,6 @@ import java.util.stream.Stream;
 @Mixin(Entity.class)
 public class MixinEntity {
     /**
-     * Redirect to allow skipping getting entities from world when the stream isn't used.
-     * The resulting stream will only evaluate the getEntityCollisions lambda when it is queried.
-     *
-     * The intended usecase are entities standing on the ground:
-     * Their gravity movement is completely blocked by the block below, querying entity collisions can't
-     * affect the movement anymore
-     */
-    @Redirect(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getEntityCollisions(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Box;Ljava/util/function/Predicate;)Ljava/util/stream/Stream;"))
-    private Stream<VoxelShape> getLazyEntityCollisionStream(World world, Entity entity_1, Box box_1, Predicate<Entity> predicate_1) {
-        Stream<Supplier<Stream<VoxelShape>>> getCollisionsLazilyStream = Stream.of(() -> entity_1.world.getEntityCollisions(entity_1, box_1, predicate_1));
-        //flatmap will only evaluate the supplier when the stream is reaching it. Won't be evaluated when block collisions cancel all movement before!
-        return getCollisionsLazilyStream.flatMap(Supplier::get);
-    }
-
-    /**
      * Redirect to try to collide with world border first, so the entity stream doesn't have to be used when other collisions cancel the whole movement already.
      */
     @Redirect(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;",
