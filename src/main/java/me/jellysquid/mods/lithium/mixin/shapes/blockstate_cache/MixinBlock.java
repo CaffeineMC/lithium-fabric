7eed57e2d66e53885cefe051ccce1ba87fbd74df
gegy1000 <gegy1000@gmail.com>
2020-06-20 09:54:28 +0200

    use faster cache implementation for isShapeFullCube check

--- src/main/java/me/jellysquid/mods/lithium/mixin/shapes/blockstate_cache/MixinBlock.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/shapes/blockstate_cache/MixinBlock.java
@@ -3,10 +3,14 @@ package me.jellysquid.mods.lithium.mixin.shapes.blockstate_cache;
 import me.jellysquid.mods.lithium.common.block.BlockShapeCacheExtended;
 import me.jellysquid.mods.lithium.common.block.BlockShapeCacheExtendedProvider;
 import me.jellysquid.mods.lithium.common.block.BlockShapeHelper;
+import me.jellysquid.mods.lithium.common.util.collections.Object2BooleanCacheTable;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,6 +22,11 @@ import org.spongepowered.asm.mixin.Overwrite;
  */
 @Mixin(Block.class)
 public class MixinBlock {
+    private static final Object2BooleanCacheTable<VoxelShape> FULL_CUBE_CACHE = new Object2BooleanCacheTable<>(
+            512,
+            shape -> !VoxelShapes.matchesAnywhere(VoxelShapes.fullCube(), shape, BooleanBiFunction.NOT_SAME)
+    );
+
     /**
      * @reason Use the shape cache
      * @author JellySquid
@@ -48,5 +57,14 @@ public class MixinBlock {
         }
 
         return BlockShapeHelper.sideCoversSquare(state.getCollisionShape(world, pos).getFace(side), BlockShapeHelper.SOLID_SMALL_SQUARE_SHAPE);
+    }
+
+    /**
+     * @reason Use a faster cache implementation
+     * @author gegy1000
+     */
+    @Overwrite
+    public static boolean isShapeFullCube(VoxelShape shape) {
+        return FULL_CUBE_CACHE.get(shape);
     }
 }
