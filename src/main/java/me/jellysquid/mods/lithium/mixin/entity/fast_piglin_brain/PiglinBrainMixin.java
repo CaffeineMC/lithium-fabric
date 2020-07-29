d08310b5f03e02ad3c6657f6a3de389cbdcfcb9b
eatmyvenom <jacobhagan@outlook.com>
2020-07-29 06:53:26 -0700

    remove unused import

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/fast_piglin_brain/PiglinBrainMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/fast_piglin_brain/PiglinBrainMixin.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
