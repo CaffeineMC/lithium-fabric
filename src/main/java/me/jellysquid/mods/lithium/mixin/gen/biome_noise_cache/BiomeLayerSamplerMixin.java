3ddba956818b2518a1422eaea72b4b0920229b6a
Robin Lindner <robin@deeprobin.de>
2020-08-23 22:39:17 +0200

    Fixed conflict

--- src/main/java/me/jellysquid/mods/lithium/mixin/gen/biome_noise_cache/BiomeLayerSamplerMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/gen/biome_noise_cache/BiomeLayerSamplerMixin.java
@@ -18,34 +18,6 @@ public abstract class BiomeLayerSamplerMixin {
         this.tlSampler = ThreadLocal.withInitial(factory::make);
     }
 
-    /**
-     * @reason Replace with implementation that accesses the thread-local sampler
-     * @author gegy1000
-     * original implementation by gegy1000, 2No2Name replaced @Overwrite with @Redirect
-     */
-    @Overwrite
-    public Biome sample(Registry<Biome> registry, int i, int j) {
-        // [VanillaCopy]
-        final CachingLayerSampler tlSampler = this.tlSampler.get();
-
-        final int k = tlSampler.sample(i, j);
-        final RegistryKey<Biome> registryKey = BuiltinBiomes.fromRawId(k);
-        if (registryKey == null) {
-            throw new IllegalStateException("Unknown biome id emitted by layers: " + k);
-        } else {
-            final Biome biome = registry.get(registryKey);
-            if (biome == null) {
-                if (SharedConstants.isDevelopment) {
-                    throw Util.throwOrPause(new IllegalStateException("Unknown biome id: " + k));
-                } else {
-                    LOGGER.warn("Unknown biome id: {}", k);
-                    return registry.get(BuiltinBiomes.fromRawId(0));
-                }
-            } else {
-                return biome;
-            }
-        
-          
     @Redirect(method = "sample", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/biome/layer/util/CachingLayerSampler;sample(II)I"))
     private int sampleThreadLocal(CachingLayerSampler cachingLayerSampler, int i, int j) {
         return this.tlSampler.get().sample(i,j);
