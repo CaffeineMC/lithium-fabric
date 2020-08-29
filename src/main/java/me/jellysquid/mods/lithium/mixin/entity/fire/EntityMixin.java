9031d0287d87245959b9e715b7dc23478aa14082
2No2Name <2No2Name@web.de>
2020-08-29 22:27:00 +0200

    Replace if else chain with more readable ternary

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/fire/EntityMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/fire/EntityMixin.java
@@ -22,15 +22,7 @@ public abstract class EntityMixin {
 
     @Redirect(method = "move", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;method_29556(Lnet/minecraft/util/math/Box;)Ljava/util/stream/Stream;"))
     private Stream<BlockState> skipFireTestIfResultDoesNotMatter(World world, Box box) {
-        if (this.fireTicks > 0) {
-            // the 2nd condition this.fireTicks <= 0 is false, so nothing ever happens.
-            return Stream.empty();
-        } else if (this.fireTicks == -this.getBurningDuration()) {
-            // no matter whether there is fire/lava or not, the value won't be changed, as it is already set to the target value.
-            return Stream.empty();
-        } else {
-            // the result of the calculation migth actually affect this.fireTicks, so do the calculation
-            return world.method_29556(box);
-        }
+        //skip scanning the blocks around the entity touches by returning an empty stream when the result does not matter
+        return this.fireTicks > 0 || this.fireTicks == -this.getBurningDuration() ? Stream.empty() : world.method_29556(box);
     }
 }
