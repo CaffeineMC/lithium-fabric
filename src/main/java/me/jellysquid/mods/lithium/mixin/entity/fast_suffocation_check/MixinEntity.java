5521aec3c25b01946c99caa255bd10a18b07d9b5
JellySquid <jellysquid3@users.noreply.github.com>
2020-08-31 19:59:28 -0500

    fix: Use less aggressive redirect for suffocation checks
    Fixes #125

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/fast_suffocation_check/MixinEntity.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/fast_suffocation_check/MixinEntity.java
@@ -1,51 +1,36 @@
 package me.jellysquid.mods.lithium.mixin.entity.fast_suffocation_check;
 
-import me.jellysquid.mods.lithium.common.entity.LithiumEntityCollisions;
 import me.jellysquid.mods.lithium.common.entity.movement.BlockCollisionPredicate;
+import me.jellysquid.mods.lithium.common.entity.movement.ChunkAwareBlockCollisionSweeper;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityDimensions;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.function.BiPredicate;
+import java.util.stream.Stream;
 
 @Mixin(Entity.class)
 public abstract class MixinEntity {
-    @Shadow
-    private EntityDimensions dimensions;
-
-    @Shadow
-    public boolean noClip;
-
-    @Shadow
-    public World world;
-
-    @Shadow
-    public abstract double getX();
-
-    @Shadow
-    public abstract double getEyeY();
-
-    @Shadow
-    public abstract double getZ();
-
     /**
      * @author JellySquid
      * @reason Use optimized block volume iteration, avoid streams
      */
-    @Overwrite
-    public boolean isInsideWall() {
-        if (this.noClip) {
-            return false;
+    @Redirect(method = "isInsideWall", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;method_30030(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Box;Ljava/util/function/BiPredicate;)Ljava/util/stream/Stream;"))
+    public Stream<VoxelShape> isInsideWall(World world, Entity entity, Box box, BiPredicate<BlockState, BlockPos> biPredicate) {
+        final ChunkAwareBlockCollisionSweeper sweeper = new ChunkAwareBlockCollisionSweeper(world, (Entity) (Object) this, box,
+                BlockCollisionPredicate.SUFFOCATES);
+        final VoxelShape shape = sweeper.getNextCollidedShape();
+
+        if (shape != null) {
+            return Stream.of(shape);
         }
 
-        float width = this.dimensions.width * 0.8F;
-
-        Box box = Box.method_30048(width, 0.1D, width)
-                .offset(this.getX(), this.getEyeY(), this.getZ());
-
-        return LithiumEntityCollisions.doesBoxCollideWithBlocks(this.world, (Entity) (Object) this, box,
-                BlockCollisionPredicate.SUFFOCATES);
+        return Stream.empty();
     }
 }
