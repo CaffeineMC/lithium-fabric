706576599990bb95085163797602b7ae5603e8b5
2No2Name <2No2Name@web.de>
2020-09-04 13:27:53 +0200

    change: code cleanup

--- src/main/java/me/jellysquid/mods/lithium/common/entity/EntityClassGroupHelper.java
+++ src/main/java/me/jellysquid/mods/lithium/common/entity/EntityClassGroupHelper.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static net.minecraft.predicate.entity.EntityPredicates.EXCEPT_SPECTATOR;
 
 public class EntityClassGroupHelper {
-    //check if the mixin of typefilterablelist is loaded, otherwise don't use it
+    //check if the EntityClassGroup Mixin of TypeFilterableList is loaded, otherwise use the vanilla implementation
     public interface MixinLoadTest {}
     public static final boolean CUSTOM_TYPE_FILTERABLE_LIST_DISABLED = !MixinLoadTest.class.isAssignableFrom(TypeFilterableList.class);
 
@@ -23,22 +23,17 @@ public class EntityClassGroupHelper {
      * for both of these methods, getting those is not necessary. This is why we only get entities when they overwrite
      * getCollisionBox
      * @param entityView the world
-     * @param selection the box the entities have to collide with
-     * @param entity the entity that is searching for the colliding entities
+     * @param box the box the entities have to collide with
+     * @param collidingEntity the entity that is searching for the colliding entities
      * @return list of entities with collision boxes
      */
-    public static List<Entity> getEntitiesWithCollisionBoxForEntity(EntityView entityView, Box selection, Entity entity) {
-        if (CUSTOM_TYPE_FILTERABLE_LIST_DISABLED || entity != null && EntityClassGroup.HARD_COLLISION_BOX_OVERRIDE.contains(entity.getClass()) || !(entityView instanceof World)) {
+    public static List<Entity> getEntitiesWithCollisionBoxForEntity(EntityView entityView, Box box, Entity collidingEntity) {
+        if (CUSTOM_TYPE_FILTERABLE_LIST_DISABLED || collidingEntity != null && EntityClassGroup.HARD_COLLISION_BOX_OVERRIDE.contains(collidingEntity.getClass()) || !(entityView instanceof World)) {
             //use vanilla code when method_30949 (previously getHardCollisionBox(Entity other)) is overwritten, as every entity could be relevant as argument of getHardCollisionBox
-            return entityView.getOtherEntities(entity, selection);
+            return entityView.getOtherEntities(collidingEntity, box);
         } else {
             //only get entities that overwrite method_30948 (previously getCollisionBox)
-            return WorldHelper.getEntitiesOfClassGroup((World)entityView, entity, EntityClassGroup.COLLISION_BOX_OVERRIDE, selection, EXCEPT_SPECTATOR);
+            return WorldHelper.getEntitiesOfClassGroup((World)entityView, collidingEntity, EntityClassGroup.COLLISION_BOX_OVERRIDE, box, EXCEPT_SPECTATOR);
         }
     }
-
-    /**
-     * Interface to group entity types that don't always return null on getCollisionBox.
-     */
-    public interface CollisionBoxOverridingEntity {}
 }
