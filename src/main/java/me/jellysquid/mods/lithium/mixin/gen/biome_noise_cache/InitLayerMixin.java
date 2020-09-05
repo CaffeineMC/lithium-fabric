af1218fc494f358b5e8e29de3777dd3b85a54853
gegy1000 <gegy1000@gmail.com>
2020-09-06 00:34:14 +0200

    apply thread-local cache to InitLayer and MergingLayer

--- src/main/java/me/jellysquid/mods/lithium/mixin/gen/biome_noise_cache/InitLayerMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/gen/biome_noise_cache/InitLayerMixin.java
@@ -2,7 +2,7 @@ package me.jellysquid.mods.lithium.mixin.gen.biome_noise_cache;
 
 import me.jellysquid.mods.lithium.common.world.layer.CachedLocalLayerFactory;
 import me.jellysquid.mods.lithium.common.world.layer.CloneableContext;
-import net.minecraft.world.biome.layer.type.ParentedLayer;
+import net.minecraft.world.biome.layer.type.InitLayer;
 import net.minecraft.world.biome.layer.util.LayerFactory;
 import net.minecraft.world.biome.layer.util.LayerSampleContext;
 import net.minecraft.world.biome.layer.util.LayerSampler;
@@ -12,15 +12,15 @@ import org.spongepowered.asm.mixin.Overwrite;
 /**
  * Memoize the LayerFactory and make it produce thread-local copies for thread-safety purposes
  */
-@Mixin(ParentedLayer.class)
-public interface ParentedLayerMixin extends ParentedLayer {
+@Mixin(InitLayer.class)
+public interface InitLayerMixin extends InitLayer {
     /**
      * @reason Replace with a memoized and thread-local layer factory
      * @author gegy1000
      */
     @Overwrite
     @SuppressWarnings("unchecked")
-    default <R extends LayerSampler> LayerFactory<R> create(LayerSampleContext<R> context, LayerFactory<R> parent) {
-        return CachedLocalLayerFactory.createParented(this, (CloneableContext<R>) context, parent);
+    default <R extends LayerSampler> LayerFactory<R> create(LayerSampleContext<R> context) {
+        return CachedLocalLayerFactory.createInit(this, (CloneableContext<R>) context);
     }
 }
