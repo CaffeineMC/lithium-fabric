872328a70ce1c9abf48113aa64604d095689ddeb
2No2Name <2No2Name@web.de>
2020-09-30 18:32:34 +0200

    new: minecarts only look at minecarts when trying to only push minecarts

---
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/replace_entitytype_predicates/AbstractMinecartEntityMixin.java
@@ -0,0 +1,20 @@
+package me.jellysquid.mods.lithium.mixin.entity.replace_entitytype_predicates;
+
+import me.jellysquid.mods.lithium.common.world.WorldHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.List;
+
+@Mixin(AbstractMinecartEntity.class)
+public class AbstractMinecartEntityMixin {
+    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getOtherEntities(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Box;)Ljava/util/List;"))
+    private List<Entity> getOtherAbstractMinecarts(World world, Entity except, Box box) {
+        return WorldHelper.getEntitiesOfClass(world, except, AbstractMinecartEntity.class, box);
+    }
+}
