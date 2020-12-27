422025cc242cc294ca1092d25bb6eec55894c949
SuperCoder7979 <k.pranav@gmail.com>
2020-12-27 09:50:18 -0500

    use fast getChunk in setBlockState and getTopY

--- src/main/java/me/jellysquid/mods/lithium/mixin/gen/chunk_region/ChunkRegionMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/gen/chunk_region/ChunkRegionMixin.java
@@ -6,6 +6,8 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.ChunkRegion;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.WorldView;
 import net.minecraft.world.chunk.Chunk;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,6 +15,7 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
@@ -55,6 +58,39 @@ public abstract class ChunkRegionMixin {
             return this.chunksArr[x + z * w].getBlockState(pos);
         } else {
             throw new NullPointerException("No chunk exists at " + new ChunkPos(pos));
+        }
+    }
+
+    /**
+     * @reason Use the chunk array for faster access
+     */
+    @Redirect(method = "setBlockState", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ChunkRegion;getChunk(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/world/chunk/Chunk;"), remap = true)
+    private Chunk fastGetChunk(ChunkRegion world, BlockPos pos) {
+        int x = (pos.getX() >> 4) - this.minChunkX;
+        int z = (pos.getZ() >> 4) - this.minChunkZ;
+        int w = this.width;
+
+        if (x >= 0 && z >= 0 && x < w && z < w) {
+            return this.chunksArr[x + z * w];
+        } else {
+            throw new NullPointerException("No chunk exists at " + new ChunkPos(pos));
+        }
+    }
+
+    /**
+     * @reason Avoid pointer de-referencing, make method easier to inline
+     * @author SuperCoder79
+     */
+    @Overwrite
+    public int getTopY(Heightmap.Type heightmap, int x, int z) {
+        int chunkX = (x >> 4) - this.minChunkX;
+        int chunkZ = (z >> 4) - this.minChunkZ;
+        int w = this.width;
+
+        if (chunkX >= 0 && chunkZ >= 0 && chunkX < w && chunkZ < w) {
+            return this.chunksArr[chunkX + chunkZ * w].sampleHeightmap(heightmap, x & 15, z & 15) + 1;
+        } else {
+            throw new NullPointerException("No chunk exists at " + new ChunkPos(chunkX, chunkZ));
         }
     }
 
