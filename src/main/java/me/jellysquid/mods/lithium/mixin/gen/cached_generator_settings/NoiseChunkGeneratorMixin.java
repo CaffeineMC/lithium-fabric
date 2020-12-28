3afa17bd8c4236a2d635d594345a9099e23bfba0
SuperCoder7979 <k.pranav@gmail.com>
2020-12-27 22:35:18 -0500

    remove Lazy in favor of caching the sea level directly

--- src/main/java/me/jellysquid/mods/lithium/mixin/gen/cached_generator_settings/NoiseChunkGeneratorMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/gen/cached_generator_settings/NoiseChunkGeneratorMixin.java
@@ -2,8 +2,10 @@ package me.jellysquid.mods.lithium.mixin.gen.cached_generator_settings;
 
 import java.util.function.Supplier;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,21 +16,23 @@ import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
 
 @Mixin(NoiseChunkGenerator.class)
 public class NoiseChunkGeneratorMixin {
-    private Lazy<ChunkGeneratorSettings> chunkGeneratorSettings;
-
-    @Inject(method = "<init>(Lnet/minecraft/world/biome/source/BiomeSource;Lnet/minecraft/world/biome/source/BiomeSource;JLjava/util/function/Supplier;)V", at = @At("RETURN"))
-    private void initializeLazy(BiomeSource populationSource, BiomeSource biomeSource, long seed, Supplier<ChunkGeneratorSettings> settings, CallbackInfo ci) {
-        this.chunkGeneratorSettings = new Lazy<>(settings);
-    }
+    @Shadow
+    @Final
+    protected Supplier<ChunkGeneratorSettings> settings;
+    private int cachedSeaLevel = Integer.MIN_VALUE;
 
     /**
-     * Use cached chunk generator settings instead of retrieving from the registry every time.
+     * Use cached sea level instead of retrieving from the registry every time.
      * This method is called for every block in the chunk so this will save a lot of registry lookups.
      *
      * @author SuperCoder79
      */
     @Overwrite
     public int getSeaLevel() {
-        return this.chunkGeneratorSettings.get().getSeaLevel();
+        if (this.cachedSeaLevel == Integer.MIN_VALUE) {
+            this.cachedSeaLevel = this.settings.get().getSeaLevel();
+        }
+
+        return this.cachedSeaLevel;
     }
 }
