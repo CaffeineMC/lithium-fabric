687119f5612fa45f1bcf7389b6a62c1906f51104
jellysquid3 <jellysquid3@users.noreply.github.com>
2021-01-08 21:24:59 -0600

    new: Replace the sine table in MathHelper with a more compact implementation
    Helps to reduce cache misses without changing values returned by MathHelper functions

---
+++ src/main/java/me/jellysquid/mods/lithium/mixin/math/sine_lut/MixinMathHelper.java
@@ -0,0 +1,25 @@
+package me.jellysquid.mods.lithium.mixin.math.sine_lut;
+
+import me.jellysquid.mods.lithium.common.util.math.CompactSineLUT;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(MathHelper.class)
+public class MixinMathHelper {
+    /**
+     * @author jellysquid3
+     */
+    @Overwrite
+    public static float sin(float f) {
+        return CompactSineLUT.sin(f);
+    }
+
+    /**
+     * @author jellysquid3
+     */
+    @Overwrite
+    public static float cos(float f) {
+        return CompactSineLUT.cos(f);
+    }
+}
