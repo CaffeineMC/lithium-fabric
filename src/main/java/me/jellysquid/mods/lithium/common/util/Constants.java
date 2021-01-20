d95e14918d82af7e46b5e50a447091dc3e44a958
2No2Name <2No2Name@web.de>
2021-01-20 14:50:43 +0100

    new: add Constants class

---
+++ src/main/java/me/jellysquid/mods/lithium/common/util/Constants.java
@@ -0,0 +1,11 @@
+package me.jellysquid.mods.lithium.common.util;
+
+public class Constants {
+    //World height constants will have to be replaced with the worldheight changes in 1.17
+    public static final int WORLD_MIN_Y = 0;
+    public static final int WORLD_MAX_Y = 255;
+    public static final int WORLD_HEIGHT = 256;
+    //World height constants >> 4
+    public static final int MIN_CHUNK_Y = 0;
+    public static final int MAX_CHUNK_Y = 15;
+}
