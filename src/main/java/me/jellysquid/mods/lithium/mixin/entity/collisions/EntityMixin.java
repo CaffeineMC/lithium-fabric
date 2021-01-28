6f48289d824f0a184885aa87adc2e45858b0c1f1
2No2Name <2No2Name@web.de>
2021-01-28 15:14:00 +0100

    change: Refactor world border collision test to avoid double test and stream concatenation

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/EntityMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/EntityMixin.java
@@ -12,6 +12,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.stream.Stream;
+
 /**
  * Replaces collision testing methods against the world border with faster checks.
  */
@@ -24,23 +26,70 @@ public abstract class EntityMixin {
     public abstract Box getBoundingBox();
 
     /**
+     * Skips the matchesAnywhere evaluation, which is replaced with {@link EntityMixin#fastWorldBorderTest(Stream, Stream, Vec3d)}.
+     */
+    @Redirect(
+            method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/util/shape/VoxelShapes;matchesAnywhere(Lnet/minecraft/util/shape/VoxelShape;Lnet/minecraft/util/shape/VoxelShape;Lnet/minecraft/util/function/BooleanBiFunction;)Z"
+            )
+    )
+    private boolean skipWorldBorderMatchesAnywhere(VoxelShape borderShape, VoxelShape entityShape, BooleanBiFunction func, Vec3d motion) {
+        return false;
+    }
+
+    /**
+     * Skip creation of unused cuboid shape
+     */
+    @Redirect(
+            method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/util/shape/VoxelShapes;cuboid(Lnet/minecraft/util/math/Box;)Lnet/minecraft/util/shape/VoxelShape;",
+                    ordinal = 0
+            )
+    )
+    private VoxelShape skipCuboid(Box box) {
+        return null;
+    }
+
+    /**
+     * Skip creation of unused stream
+     */
+    @Redirect(
+            method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/stream/Stream;empty()Ljava/util/stream/Stream;",
+                    ordinal = 0
+            )
+    )
+    private Stream<VoxelShape> skipStream() {
+        return null;
+    }
+
+    /**
      * Uses a very quick check to determine if the player is outside the world border (which would disable collisions
      * against it). We also perform an additional check to see if the player can even collide with the world border in
      * this physics step, allowing us to remove it from collision testing in later code.
      *
-     * @return True if no collision resolution will be performed against the world border, which removes it from the
-     * stream of shapes to consider in entity collision code.
+     * @return The combined entity shapes and worldborder stream, or if the worldborder cannot be collided with the entity stream will be returned.
      */
-    @Redirect(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/shape/VoxelShapes;matchesAnywhere(Lnet/minecraft/util/shape/VoxelShape;Lnet/minecraft/util/shape/VoxelShape;Lnet/minecraft/util/function/BooleanBiFunction;)Z"))
-    private boolean redirectWorldBorderMatchesAnywhere(VoxelShape borderShape, VoxelShape entityShape, BooleanBiFunction func, Vec3d motion) {
-        boolean isWithinWorldBorder = LithiumEntityCollisions.isWithinWorldBorder(this.world.getWorldBorder(), this.getBoundingBox().contract(1.0E-7D));
-
-        // If the entity is within the world border (enabling collisions against it), check that the player will cross the
-        // border this physics step.
-        if (isWithinWorldBorder) {
-            return LithiumEntityCollisions.isWithinWorldBorder(this.world.getWorldBorder(), this.getBoundingBox().stretch(motion));
+    @Redirect(
+            method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/stream/Stream;concat(Ljava/util/stream/Stream;Ljava/util/stream/Stream;)Ljava/util/stream/Stream;"
+            )
+    )
+    private Stream<VoxelShape> fastWorldBorderTest(Stream<VoxelShape> entityShapes, Stream<VoxelShape> emptyStream, Vec3d motion) {
+        if (LithiumEntityCollisions.isWithinWorldBorder(this.world.getWorldBorder(), this.getBoundingBox().stretch(motion)) ||
+                !LithiumEntityCollisions.isWithinWorldBorder(this.world.getWorldBorder(), this.getBoundingBox().contract(1.0E-7D))) {
+            return entityShapes;
         }
-
-        return true;
+        //the world border shape will only be collided with, if the entity is colliding with the world border after movement but is not
+        //colliding with the world border already
+        return Stream.concat(entityShapes, Stream.of(this.world.getWorldBorder().asVoxelShape()));
     }
 }
--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/stream_entity_collisions_lazily/EntityMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/stream_entity_collisions_lazily/EntityMixin.java
@@ -11,16 +11,6 @@ import java.util.stream.Stream;
 @Mixin(Entity.class)
 public class EntityMixin {
     /**
-     * Redirect to try to collide with world border first, so the entity stream doesn't have to be used when other collisions cancel the whole movement already.
-     */
-    @Redirect(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;",
-            at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;concat(Ljava/util/stream/Stream;Ljava/util/stream/Stream;)Ljava/util/stream/Stream;"))
-    private Stream<VoxelShape> reorderStreams_WorldBorderCollisionsFirst(Stream<? extends VoxelShape> entityShapes, Stream<? extends VoxelShape> blockShapes) {
-        return Stream.concat(blockShapes, entityShapes);
-    }
-
-
-    /**
      * Redirect to try to collide with blocks first, so the entity stream doesn't have to be used when block collisions cancel the whole movement already.
      */
     @Redirect(method = "adjustMovementForCollisions(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Box;Lnet/minecraft/world/World;Lnet/minecraft/block/ShapeContext;Lnet/minecraft/util/collection/ReusableStream;)Lnet/minecraft/util/math/Vec3d;",
