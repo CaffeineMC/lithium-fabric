0c4c189806a7dcfe7139f62d7be663ee95627e0b
2No2Name <2No2Name@web.de>
2021-02-20 17:06:54 +0100

    change: add use case description to config file

--- src/main/java/me/jellysquid/mods/lithium/common/config/LithiumConfig.java
+++ src/main/java/me/jellysquid/mods/lithium/common/config/LithiumConfig.java
@@ -283,6 +283,7 @@ public class LithiumConfig {
 
         try (Writer writer = new FileWriter(file)) {
             writer.write("# This is the configuration file for Lithium.\n");
+            writer.write("# This file exists for debugging purposes and should not be configured otherwise.\n");
             writer.write("#\n");
             writer.write("# You can find information on editing this file and all the available options here:\n");
             writer.write("# https://github.com/jellysquid3/lithium-fabric/wiki/Configuration-File\n");
