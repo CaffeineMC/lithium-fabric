bb2ebf310e8cee0519bbf5263c2a3cb2e57f558f
2No2Name <2No2Name@web.de>
2021-02-20 15:19:55 +0100

    new: cache the supported block of block entities

---
+++ src/main/java/me/jellysquid/mods/lithium/mixin/world/block_entity_ticking/support_cache/BlockEntityMixin.java
@@ -0,0 +1,29 @@
+package me.jellysquid.mods.lithium.mixin.world.block_entity_ticking.support_cache;
+
+import me.jellysquid.mods.lithium.common.world.blockentity.SupportCache;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(BlockEntity.class)
+public abstract class BlockEntityMixin implements SupportCache {
+    @Shadow
+    public abstract BlockState getCachedState();
+
+    @Shadow
+    public abstract BlockEntityType<?> getType();
+
+    private BlockState supportTestState;
+    private boolean supportTestResult;
+
+    @Override
+    public boolean isSupported() {
+        BlockState cachedState = this.getCachedState();
+        if (this.supportTestState == cachedState) {
+            return this.supportTestResult;
+        }
+        return this.supportTestResult = this.getType().supports((this.supportTestState = cachedState).getBlock());
+    }
+}
