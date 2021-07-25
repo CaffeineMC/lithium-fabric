f56638fd455b315ddd1718ca75f234e07fba1995
Mitchell Skaggs <skaggsm333@gmail.com>
2021-07-25 03:39:15 -0500

    Avoid method call in fast path by using object equality
    Technically someone _could_ reflection-hack a second `Optional` object with a `null` value, but that isn't supported.

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/cached_climbing_check/MixinLivingEntity.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/cached_climbing_check/MixinLivingEntity.java
@@ -1,14 +1,17 @@
 package me.jellysquid.mods.lithium.mixin.entity.cached_climbing_check;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Optional;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity {
     @Redirect(method = "isPushable", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;isClimbing()Z"))
     boolean preventClimbingUpdateCheck(LivingEntity livingEntity) {
-        return livingEntity.getClimbingPos().isPresent() || livingEntity.isClimbing();
+        return livingEntity.getClimbingPos() != Optional.<BlockPos>empty() || livingEntity.isClimbing();
     }
 }
