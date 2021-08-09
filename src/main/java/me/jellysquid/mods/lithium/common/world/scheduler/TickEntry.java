9c7592536e7c0749e79eecfd94dbb741535af26a
2No2Name <2No2Name@web.de>
2021-08-09 13:33:35 +0200

    fix: LithiumServerTickScheduler reordering ticks in saving and cloning
    Caused by Lithium not using ordered collections in the saving/cloning code
    
    fix: LithiumServerTickScheduler causing incorrect tick information to be displayed with Redstone Debugging mods
    
    Caused by Lithium allowing outdated data to stay in fields that are not used with Lithium. Redstone Debugging tools do not expect this behavior and display this outdated data.

--- src/main/java/me/jellysquid/mods/lithium/common/world/scheduler/TickEntry.java
+++ src/main/java/me/jellysquid/mods/lithium/common/world/scheduler/TickEntry.java
@@ -1,27 +1,13 @@
 package me.jellysquid.mods.lithium.common.world.scheduler;
 
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ScheduledTick;
-
-import java.util.Set;
+import net.minecraft.world.TickPriority;
 
 /**
  * A wrapper type for {@link ScheduledTick} which adds fields to mark the state of the tick in the scheduler's pipeline.
  */
 public class TickEntry<T> extends ScheduledTick<T> {
-    /**
-     * True if the tick has been scheduled for execution. After the tick has been selected for execution during the
-     * current world tick, this is unset and the executing flag is set. The tick can then no longer be re-scheduled
-     * without creating a new tick.
-     */
-    public boolean scheduled = false;
-
-    /**
-     * True if the tick will be executed during the current world tick. After all enqueued ticks have been executed
-     * in the tick phase, this is unset. Used by redstone contraptions to know whether or not something is presently
-     * being ticked as to avoid duplicate updates.
-     */
-    public boolean executing = false;
-
     /**
      * True if the tick has been executed (and therefore consumed). This flag is set right before the tick is actually
      * performed. If this tick is re-scheduled during execution, the consumed flag will be unset and the scheduled
@@ -29,16 +15,12 @@ public class TickEntry<T> extends ScheduledTick<T> {
      */
     public boolean consumed = false;
 
-    /**
-     * A pointer to the chunk index belonging to this scheduled tick.
-     */
-    public final Set<TickEntry<T>> chunkIdx;
+    public TickEntry(BlockPos pos, T object, long time, TickPriority priority) {
+        super(pos, object, time, priority);
+    }
 
-    public TickEntry(ScheduledTick<T> tick, Set<TickEntry<T>> chunkIdx) {
-        super(tick.pos, tick.getObject(), tick.time, tick.priority);
-
-        this.chunkIdx = chunkIdx;
-        this.chunkIdx.add(this);
+    public TickEntry(BlockPos pos, T t) {
+        super(pos, t, 0L, TickPriority.NORMAL);
     }
 }
 
