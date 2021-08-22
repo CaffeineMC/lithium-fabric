6378325f7a7c390aad5c31e785d9f7dd275f7070
2No2Name <2No2Name@web.de>
2021-08-22 15:07:01 +0200

    fix: remove gen.voronoi_biomes after vanilla implemented the same optimization in 20w45a

--- src/main/java/me/jellysquid/mods/lithium/common/config/LithiumConfig.java
+++ src/main/java/me/jellysquid/mods/lithium/common/config/LithiumConfig.java
@@ -87,7 +87,6 @@ public class LithiumConfig {
         this.addMixinRule("gen.fast_noise_interpolation", true);
         this.addMixinRule("gen.features", true);
         this.addMixinRule("gen.perlin_noise", true);
-        this.addMixinRule("gen.voronoi_biomes", true);
 
         this.addMixinRule("math", true);
         this.addMixinRule("math.fast_blockpos", true);
