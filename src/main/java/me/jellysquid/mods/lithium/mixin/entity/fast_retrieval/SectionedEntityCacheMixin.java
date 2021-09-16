b34ac353bc6bd97d1b436898f353f998d29626a4
2No2Name <2No2Name@web.de>
2021-09-16 19:58:59 +0200

    fix: entity retrieval uses different section order around both z = 0 and y = 0

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/fast_retrieval/SectionedEntityCacheMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/fast_retrieval/SectionedEntityCacheMixin.java
@@ -30,15 +30,41 @@ public abstract class SectionedEntityCacheMixin<T> {
         int maxY = ChunkSectionPos.getSectionCoord(box.maxY + 2.0D);
         int maxZ = ChunkSectionPos.getSectionCoord(box.maxZ + 2.0D);
 
+
+        // Vanilla order of the AVL long set is sorting by ascending long value. The x, y, z positions are packed into
+        // a long with the x position's lowest 22 bits placed at the MSB.
+        // Therefore the long is negative iff the 22th bit of the x position is set, which happens iff the x position
+        // is negative. A positive x position will never have its 22th bit set, as these big coordinates are far outside
+        // the world. y and z positions are treated as unsigned when sorting by ascending long value, as their sign bits
+        // are placed somewhere inside the packed long
+
         for (int x = minX; x <= maxX; x++) {
-            for (int z = minZ; z <= maxZ; z++) {
-                for (int y = minY; y <= maxY; y++) {
-                    EntityTrackingSection<T> section = this.findTrackingSection(ChunkSectionPos.asLong(x, y, z));
-                    if (section != null && section.getStatus().shouldTrack()) {
-                        action.accept(section);
-                    }
-                }
+            for (int z = Math.max(minZ, 0); z <= maxZ; z++) {
+                this.forEachInColumn(x, minY, maxY, z, action);
             }
+
+            int bound = Math.min(-1, maxZ);
+            for (int z = minY; z <= bound; z++) {
+                this.forEachInColumn(x, minY, maxY, z, action);
+            }
+        }
+    }
+
+    private void forEachInColumn(int x, int minY, int maxY, int z, Consumer<EntityTrackingSection<T>> action) {
+        //y from negative to positive, but y is treated as unsigned
+        for (int y = Math.max(minY, 0); y <= maxY; y++) {
+            this.consumeSection(x, y, z, action);
+        }
+        int bound = Math.min(-1, maxY);
+        for (int y = minY; y <= bound; y++) {
+            this.consumeSection(x, y, z, action);
+        }
+    }
+
+    private void consumeSection(int x, int y, int z, Consumer<EntityTrackingSection<T>> action) {
+        EntityTrackingSection<T> section = this.findTrackingSection(ChunkSectionPos.asLong(x, y, z));
+        if (section != null && section.getStatus().shouldTrack()) {
+            action.accept(section);
         }
     }
 }
