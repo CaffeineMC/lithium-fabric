25c90caa50f4b9a2aba705fae26470ef55904289
2No2Name <2No2Name@web.de>
2021-11-16 22:49:33 +0100

    change: remove incompatible layer caching code

--- src/main/java/me/jellysquid/mods/lithium/common/world/layer/CloneableContext.java
+++
@@ -1,8 +0,0 @@
-package me.jellysquid.mods.lithium.common.world.layer;
-
-import net.minecraft.world.biome.layer.util.LayerSampleContext;
-import net.minecraft.world.biome.layer.util.LayerSampler;
-
-public interface CloneableContext<R extends LayerSampler> {
-    LayerSampleContext<R> cloneContext();
-}
