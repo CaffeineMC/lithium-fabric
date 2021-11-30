90815f9583021a8391d2ebce7d0ba748e7f45972
2No2Name <2No2Name@web.de>
2021-11-30 18:20:57 +0100

    change: remove optimization that is also in vanilla

--- src/main/java/me/jellysquid/mods/lithium/mixin/math/fast_util/DirectionMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/math/fast_util/DirectionMixin.java
@@ -6,8 +6,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.Random;
-
 @Mixin(Direction.class)
 public class DirectionMixin {
     @Shadow
@@ -25,14 +23,5 @@ public class DirectionMixin {
     @Overwrite
     public Direction getOpposite() {
         return ALL[this.idOpposite];
-    }
-
-    /**
-     * @reason Do not allocate an excessive number of Direction arrays
-     * @author JellySquid
-     */
-    @Overwrite
-    public static Direction random(Random rand) {
-        return ALL[rand.nextInt(ALL.length)];
     }
 }
