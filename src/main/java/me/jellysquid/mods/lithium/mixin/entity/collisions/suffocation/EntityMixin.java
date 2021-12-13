71fb4f4d9b23071b40a5ddc2949ef9b217f0c458
2No2Name <2No2Name@web.de>
2021-12-13 11:12:09 +0100

    fix: entity.collisions.suffocation using wrong eye Box for block collision check

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/suffocation/EntityMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/suffocation/EntityMixin.java
@@ -39,17 +39,23 @@ public abstract class EntityMixin {
         if (this.noClip) {
             return false;
         }
-        Vec3d vec3d = this.getEyePos();
+        Vec3d eyePos = this.getEyePos();
         double suffocationRadius = Math.abs((double) (this.dimensions.width * 0.8f) / 2.0);
 
-        BlockPos incorrectSuffocationPos = new BlockPos(vec3d);
+        BlockPos incorrectSuffocationPos = new BlockPos(eyePos);
 
-        int minX = MathHelper.floor(vec3d.x - suffocationRadius);
-        int minY = MathHelper.floor(vec3d.y - 5.0E-7);
-        int minZ = MathHelper.floor(vec3d.z - suffocationRadius);
-        int maxX = MathHelper.floor(vec3d.x + suffocationRadius);
-        int maxY = MathHelper.floor(vec3d.y + 5.0E-7);
-        int maxZ = MathHelper.floor(vec3d.z + suffocationRadius);
+        double suffocationMinX = eyePos.x - suffocationRadius;
+        double suffocationMinY = eyePos.y - 5.0E-7;
+        double suffocationMinZ = eyePos.z - suffocationRadius;
+        double suffocationMaxX = eyePos.x + suffocationRadius;
+        double suffocationMaxY = eyePos.y + 5.0E-7;
+        double suffocationMaxZ = eyePos.z + suffocationRadius;
+        int minX = MathHelper.floor(suffocationMinX);
+        int minY = MathHelper.floor(suffocationMinY);
+        int minZ = MathHelper.floor(suffocationMinZ);
+        int maxX = MathHelper.floor(suffocationMaxX);
+        int maxY = MathHelper.floor(suffocationMaxY);
+        int maxZ = MathHelper.floor(suffocationMaxZ);
 
         World world = this.world;
         BlockPos.Mutable blockPos = new BlockPos.Mutable();
@@ -63,9 +69,11 @@ public abstract class EntityMixin {
                     // [VanillaCopy] https://bugs.mojang.com/browse/MC-242543 (incorrectly marked as cannot reproduce)
                     if (!blockState.isAir() && blockState.shouldSuffocate(this.world, incorrectSuffocationPos)) {
                         if (suffocationShape == null) {
-                            suffocationShape = VoxelShapes.cuboid(new Box(minX, minY, minZ, maxX, maxY, maxZ));
+                            suffocationShape = VoxelShapes.cuboid(new Box(suffocationMinX, suffocationMinY, suffocationMinZ, suffocationMaxX, suffocationMaxY, suffocationMaxZ));
                         }
-                        if (VoxelShapes.matchesAnywhere(blockState.getCollisionShape(this.world, incorrectSuffocationPos).offset(vec3d.x, vec3d.y, vec3d.z), suffocationShape, BooleanBiFunction.AND)) {
+                        if (VoxelShapes.matchesAnywhere(blockState.getCollisionShape(this.world, incorrectSuffocationPos).
+                                        offset(eyePos.x, eyePos.y, eyePos.z), // [VanillaCopy] This is broken. Should be offset by blockPos. But vanilla does it like this. Causes https://bugs.mojang.com/browse/MC-245416
+                                suffocationShape, BooleanBiFunction.AND)) {
                             return true;
                         }
                     }
