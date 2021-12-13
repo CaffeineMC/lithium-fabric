a220f5f2cb0851bd77a0e176896b20d6580a1d89
Maity <47220489+maityyy@users.noreply.github.com>
2021-12-13 21:30:22 +0300

    Replace collection everywhere

--- src/main/java/me/jellysquid/mods/lithium/mixin/collections/nbt/NbtCompoundMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/collections/nbt/NbtCompoundMixin.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.HashMap;
@@ -47,5 +48,34 @@ public class NbtCompoundMixin {
         // [VanillaCopy] HashMap is replaced with Object2ObjectOpenHashMap
         var map = new Object2ObjectOpenHashMap<>(Maps.transformValues(this.entries, NbtElement::copy));
         return new NbtCompound(map);
+    }
+
+    @Mixin(targets = "net.minecraft.nbt.NbtCompound$1")
+    static class Type {
+
+        @ModifyVariable(
+                method = "read(Ljava/io/DataInput;ILnet/minecraft/nbt/NbtTagSizeTracker;)Lnet/minecraft/nbt/NbtCompound;",
+                at = @At(
+                        value = "INVOKE_ASSIGN",
+                        target = "Lcom/google/common/collect/Maps;newHashMap()Ljava/util/HashMap;",
+                        remap = false
+                )
+        )
+        private Map<String, NbtElement> useFasterCollection(Map<String, NbtElement> map) {
+            return new Object2ObjectOpenHashMap<>();
+        }
+
+
+        @Redirect(
+                method = "read(Ljava/io/DataInput;ILnet/minecraft/nbt/NbtTagSizeTracker;)Lnet/minecraft/nbt/NbtCompound;",
+                at = @At(
+                        value = "INVOKE",
+                        target = "Lcom/google/common/collect/Maps;newHashMap()Ljava/util/HashMap;",
+                        remap = false
+                )
+        )
+        private <K, V> HashMap<K, V> removeOldMapAlloc() {
+            return null;
+        }
     }
 }
