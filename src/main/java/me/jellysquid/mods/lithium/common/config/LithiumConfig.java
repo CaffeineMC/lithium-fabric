3dad86ace34218482e39b5e77ab2cb40eec753a2
2No2Name <2No2Name@web.de>
2021-12-17 03:05:45 +0100

    fix: remove outdated chunk.section_update_tracking after being added to vanilla

--- src/main/java/me/jellysquid/mods/lithium/common/config/LithiumConfig.java
+++ src/main/java/me/jellysquid/mods/lithium/common/config/LithiumConfig.java
@@ -68,7 +68,6 @@ public class LithiumConfig {
         this.addMixinRule("chunk.no_locking", true);
         this.addMixinRule("chunk.no_validation", true);
         this.addMixinRule("chunk.palette", true);
-        this.addMixinRule("chunk.section_update_tracking", true);
         this.addMixinRule("chunk.serialization", true);
 
         this.addMixinRule("collections", true);
