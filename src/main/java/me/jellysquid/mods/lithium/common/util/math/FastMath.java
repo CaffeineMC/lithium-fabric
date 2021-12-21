e99b06763190cccf9872256ec63c4ce23ff50fa3
FX - PR0CESS <fx.e.morin@gmail.com>
2021-12-21 15:33:53 -0500

    fix: @link format

--- src/main/java/me/jellysquid/mods/lithium/common/util/math/FastMath.java
+++ src/main/java/me/jellysquid/mods/lithium/common/util/math/FastMath.java
@@ -5,7 +5,7 @@ public class FastMath {
     /**
      * @author FX - PR0CESS
      * Custom rounding method which is even faster while keeping 100% accuracy
-     * ~1.25x faster than @link{Math#round(float)}
+     * ~1.25x faster than {@link Math#round(float)}
      */
     public static int round(float a) {
         return a > 0F ? (int)(a + .5F) : (int)(a - .5F);
@@ -14,7 +14,7 @@ public class FastMath {
     /**
      * @author FX - PR0CESS
      * Custom rounding method which is even faster while keeping 100% accuracy
-     * ~1.28x faster than @link{Math#round(double)}
+     * ~1.28x faster than {@link Math#round(double)}
      */
     public static long round(double a) {
         return a > 0D ? (long)(a + .5D) : (long)(a - .5D);
