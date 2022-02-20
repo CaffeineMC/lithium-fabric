d022f88d8ad7fca71405c6f833b7e160147c0b10
malte0811 <malte0811@web.de>
2022-02-20 08:38:31 +0100

    Set the LockHelper field in PalettedContainer to null
    The LockHelper is completly unused with the no_locking mixin, and
    accounts for 25+% of the memory used by chunks in practice.

--- src/main/java/me/jellysquid/mods/lithium/mixin/chunk/no_locking/PalettedContainerMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/chunk/no_locking/PalettedContainerMixin.java
@@ -1,8 +1,15 @@
 package me.jellysquid.mods.lithium.mixin.chunk.no_locking;
 
+import net.minecraft.util.thread.LockHelper;
 import net.minecraft.world.chunk.PalettedContainer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * The implementation of {@link PalettedContainer} performs a strange check to catch concurrent modification and throws
@@ -21,6 +28,23 @@ import org.spongepowered.asm.mixin.Overwrite;
  */
 @Mixin(PalettedContainer.class)
 public class PalettedContainerMixin {
+    @Shadow
+    @Final
+    @Mutable
+    private LockHelper lockHelper;
+
+    @Inject(
+            method = {
+                    "<init>(Lnet/minecraft/util/collection/IndexedIterable;Ljava/lang/Object;Lnet/minecraft/world/chunk/PalettedContainer$PaletteProvider;)V",
+                    "<init>(Lnet/minecraft/util/collection/IndexedIterable;Lnet/minecraft/world/chunk/PalettedContainer$PaletteProvider;Lnet/minecraft/world/chunk/PalettedContainer$Data;)V",
+                    "<init>(Lnet/minecraft/util/collection/IndexedIterable;Lnet/minecraft/world/chunk/PalettedContainer$PaletteProvider;Lnet/minecraft/world/chunk/PalettedContainer$DataProvider;Lnet/minecraft/util/collection/PaletteStorage;Ljava/util/List;)V",
+            },
+            at = @At("TAIL")
+    )
+    public void removeLockHelper(CallbackInfo ci) {
+        this.lockHelper = null;
+    }
+
     /**
      * @reason Do not check the container's lock
      * @author JellySquid
