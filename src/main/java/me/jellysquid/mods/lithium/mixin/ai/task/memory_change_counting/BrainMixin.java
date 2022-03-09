f43cb4e28d5c0e2a3322eb4b4eb92e7ebe557774
2No2Name <2No2Name@web.de>
2022-03-09 16:29:37 +0100

    fix: ai.task.memory_change_counting: copy memory change count to copied brains fix: ai.task.memory_change_counting: do not use cached values before setting them

--- src/main/java/me/jellysquid/mods/lithium/mixin/ai/task/memory_change_counting/BrainMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/ai/task/memory_change_counting/BrainMixin.java
@@ -4,7 +4,9 @@ import me.jellysquid.mods.lithium.common.ai.MemoryModificationCounter;
 import net.minecraft.entity.ai.brain.Brain;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 import java.util.Optional;
@@ -32,5 +34,19 @@ public class BrainMixin implements MemoryModificationCounter {
     @Override
     public long getModCount() {
         return memoryModCount;
+    }
+
+
+    /**
+     * Fix mod count being reset when villager loses profession due to disappearing workstation.
+     * Mod count being reset can lead to tasks not running even though they should be!
+     */
+    @Inject(
+            method = "copy()Lnet/minecraft/entity/ai/brain/Brain;",
+            at = @At("RETURN")
+    )
+    private void copyModCount(CallbackInfoReturnable<Brain<?>> cir) {
+        Brain<?> newBrain = cir.getReturnValue();
+        ((BrainMixin) (Object) newBrain).memoryModCount = this.memoryModCount + 1;
     }
 }
