20b16440fd455dc77218e56e420b053aad4a559f
2No2Name <2No2Name@web.de>
2022-03-15 06:58:57 +0100

    fix: block.hopper: hopper not updating after external modification of "shadow items"
    Shadow items are item stacks that are in multiple inventories at the same time (requires obscure bug usage). Changing the stack in one inventory will modify the stack in another inventory, because it is the same mutable object being in both inventories. Therefore, caches relying on the item stacks inside the inventory being only accessed through the inventory are not invalidated correctly.
    
    The fix circumvents this issue by disabling the caches for all inventories that at some point in their lifetime contained such a "shadow item".

--- src/main/java/me/jellysquid/mods/lithium/mixin/block/hopper/ItemStackMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/block/hopper/ItemStackMixin.java
@@ -3,6 +3,7 @@ package me.jellysquid.mods.lithium.mixin.block.hopper;
 import me.jellysquid.mods.lithium.common.hopper.LithiumStackList;
 import me.jellysquid.mods.lithium.common.hopper.StorableItemStack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,6 +12,8 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin implements StorableItemStack {
+
+    private static final LithiumStackList DUMMY_STACK_LIST = new LithiumStackList(DefaultedList.of(), 0);
     @Shadow
     private int count;
 
@@ -21,20 +24,28 @@ public abstract class ItemStackMixin implements StorableItemStack {
 
     @Override
     public void registerToInventory(LithiumStackList itemStacks, int mySlot) {
-        assert this.myLocation == null;
-        this.myLocation = itemStacks;
-        this.mySlot = mySlot;
+        if (this.myLocation != null) {
+            this.myLocation.setUnstableInteractionConditions();
+            itemStacks.setUnstableInteractionConditions();
+            this.myLocation = DUMMY_STACK_LIST;
+        } else {
+            this.myLocation = itemStacks;
+            this.mySlot = mySlot;
+        }
     }
 
     @Override
     public void unregisterFromInventory(LithiumStackList myInventoryList) {
+        if (myInventoryList == DUMMY_STACK_LIST) {
+            return;
+        }
         assert this.myLocation == myInventoryList;
         this.myLocation = null;
     }
 
     @ModifyVariable(method = "setCount(I)V", at = @At("HEAD"))
     public int updateInventory(int count) {
-        if (this.myLocation != null && this.count != count) {
+        if (this.myLocation != null && this.myLocation != DUMMY_STACK_LIST && this.count != count) {
             this.myLocation.beforeSlotCountChange(this.mySlot, count);
         }
         return count;
