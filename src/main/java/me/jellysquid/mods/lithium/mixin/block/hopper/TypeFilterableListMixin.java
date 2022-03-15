c0d871d969e69a2990ff7a2bcea9f1c3d5bfb156
2No2Name <2No2Name@web.de>
2022-03-15 07:16:46 +0100

    fix: block.hopper: set remap = false in TypeFilterableListMixin

--- src/main/java/me/jellysquid/mods/lithium/mixin/block/hopper/TypeFilterableListMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/block/hopper/TypeFilterableListMixin.java
@@ -8,7 +8,9 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(value = TypeFilterableList.class, priority = 1010)
 public class TypeFilterableListMixin<T> {
     //require = 0 due to the overwrite in the other TypeFilterableListMixin
-    @Redirect(method = "getAllOfType(Ljava/lang/Class;)Ljava/util/Collection;", at = @At(value = "INVOKE", target = "Ljava/lang/Class;isAssignableFrom(Ljava/lang/Class;)Z"), require = 0)
+    @Redirect(
+            method = "getAllOfType(Ljava/lang/Class;)Ljava/util/Collection;",
+            at = @At(value = "INVOKE", target = "Ljava/lang/Class;isAssignableFrom(Ljava/lang/Class;)Z", remap = false), require = 0)
     private boolean isAlwaysAssignable(Class<?> aClass, Class<?> cls) {
         return true;
     }
