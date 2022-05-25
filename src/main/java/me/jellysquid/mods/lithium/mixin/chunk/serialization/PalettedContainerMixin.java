0890fc93c28b28c2ccb722cb93810c729405975e
2No2Name <2No2Name@web.de>
2022-05-25 18:26:09 +0200

    Update to 1.19

--- src/main/java/me/jellysquid/mods/lithium/mixin/chunk/serialization/PalettedContainerMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/chunk/serialization/PalettedContainerMixin.java
@@ -59,7 +59,7 @@ public abstract class PalettedContainerMixin<T> {
      * @author JellySquid
      */
     @Overwrite
-    private PalettedContainer.Serialized<T> write(IndexedIterable<T> idList, PalettedContainer.PaletteProvider provider) {
+    public PalettedContainer.Serialized<T> method_44345(IndexedIterable<T> idList, PalettedContainer.PaletteProvider provider) {
         this.lock();
 
         // The palette that will be serialized
