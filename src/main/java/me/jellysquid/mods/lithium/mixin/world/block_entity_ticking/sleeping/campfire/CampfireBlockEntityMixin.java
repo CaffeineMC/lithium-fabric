2f1e7478670584c94e020621de3388a878090596
2No2Name <2No2Name@web.de>
2022-08-02 01:06:03 +0200

    new: sleeping campfires

---
+++ src/main/java/me/jellysquid/mods/lithium/mixin/world/block_entity_ticking/sleeping/campfire/CampfireBlockEntityMixin.java
@@ -0,0 +1,90 @@
+package me.jellysquid.mods.lithium.mixin.world.block_entity_ticking.sleeping.campfire;
+
+import me.jellysquid.mods.lithium.common.block.entity.SleepingBlockEntity;
+import me.jellysquid.mods.lithium.mixin.world.block_entity_ticking.sleeping.WrappedBlockEntityTickInvokerAccessor;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.entity.CampfireBlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.BlockEntityTickInvoker;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(CampfireBlockEntity.class)
+public class CampfireBlockEntityMixin extends BlockEntity implements SleepingBlockEntity {
+
+    @Shadow
+    @Final
+    private int[] cookingTimes;
+    @Shadow
+    @Final
+    private DefaultedList<ItemStack> itemsBeingCooked;
+    private WrappedBlockEntityTickInvokerAccessor tickWrapper = null;
+    private BlockEntityTickInvoker sleepingTicker = null;
+
+    public CampfireBlockEntityMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    @Override
+    public WrappedBlockEntityTickInvokerAccessor getTickWrapper() {
+        return tickWrapper;
+    }
+
+    @Override
+    public void setTickWrapper(WrappedBlockEntityTickInvokerAccessor tickWrapper) {
+        this.tickWrapper = tickWrapper;
+    }
+
+    @Override
+    public BlockEntityTickInvoker getSleepingTicker() {
+        return sleepingTicker;
+    }
+
+    @Override
+    public void setSleepingTicker(BlockEntityTickInvoker sleepingTicker) {
+        this.sleepingTicker = sleepingTicker;
+    }
+
+
+    @Inject(
+            method = "addItem",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/collection/DefaultedList;set(ILjava/lang/Object;)Ljava/lang/Object;" )
+    )
+    private void wakeUpOnAddItem(Entity user, ItemStack stack, int cookTime, CallbackInfoReturnable<Boolean> cir) {
+        this.wakeUpNow();
+    }
+
+    @Inject(
+            method = {"litServerTick", "unlitServerTick"},
+            at = @At("RETURN" ),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    private static void trySleep(World world, BlockPos pos, BlockState state, CampfireBlockEntity campfire, CallbackInfo ci, boolean hadProgress) {
+        if (!hadProgress) {
+            CampfireBlockEntityMixin self = (CampfireBlockEntityMixin) (Object) campfire;
+            for (int value : self.cookingTimes) {
+                if (value != 0) {
+                    return;
+                }
+            }
+            for (ItemStack itemStack : self.itemsBeingCooked) {
+                if (!itemStack.isEmpty()) {
+                    return;
+                }
+            }
+            self.startSleeping();
+        }
+    }
+}
