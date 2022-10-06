0a597e5f7c57e287d1a772d5f0ba21a27102c5b9
2No2Name <2No2Name@web.de>
2022-10-06 20:10:37 +0200

    change: replace suffocation overwrite with inject to improve mod compatibility

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/suffocation/EntityMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/suffocation/EntityMixin.java
@@ -12,8 +12,11 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
@@ -33,33 +36,30 @@ public abstract class EntityMixin {
      * @author 2No2Name
      * @reason Avoid stream code, use optimized chunk section iteration order
      */
-    @Overwrite
-    public boolean isInsideWall() {
-        // [VanillaCopy] The whole method functionality including bug below. Cannot use ChunkAwareBlockCollisionSweeper due to ignoring of oversized blocks
-        if (this.noClip) {
-            return false;
-        }
-        Vec3d eyePos = this.getEyePos();
-        double suffocationRadius = Math.abs((double) (this.dimensions.width * 0.8f) / 2.0);
-
-        double suffocationMinX = eyePos.x - suffocationRadius;
-        double suffocationMinY = eyePos.y - 5.0E-7;
-        double suffocationMinZ = eyePos.z - suffocationRadius;
-        double suffocationMaxX = eyePos.x + suffocationRadius;
-        double suffocationMaxY = eyePos.y + 5.0E-7;
-        double suffocationMaxZ = eyePos.z + suffocationRadius;
-        int minX = MathHelper.floor(suffocationMinX);
-        int minY = MathHelper.floor(suffocationMinY);
-        int minZ = MathHelper.floor(suffocationMinZ);
-        int maxX = MathHelper.floor(suffocationMaxX);
-        int maxY = MathHelper.floor(suffocationMaxY);
-        int maxZ = MathHelper.floor(suffocationMaxZ);
+    @Inject(
+            method = "isInsideWall", cancellable = true,
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/util/math/BlockPos;stream(Lnet/minecraft/util/math/Box;)Ljava/util/stream/Stream;",
+                    shift = At.Shift.BEFORE
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    public void isInsideWall(CallbackInfoReturnable<Boolean> cir, float f, Box box) {
+        // [VanillaCopy]
+        int minX = MathHelper.floor(box.minX);
+        int minY = MathHelper.floor(box.minY);
+        int minZ = MathHelper.floor(box.minZ);
+        int maxX = MathHelper.floor(box.maxX);
+        int maxY = MathHelper.floor(box.maxY);
+        int maxZ = MathHelper.floor(box.maxZ);
 
         World world = this.world;
         //skip getting blocks when the entity is outside the world height
         //also avoids infinite loop with entities below y = Integer.MIN_VALUE (some modded servers do that)
         if (world.getBottomY() > maxY || world.getTopY() < minY) {
-            return false;
+            cir.setReturnValue(false);
+            return;
         }
 
         BlockPos.Mutable blockPos = new BlockPos.Mutable();
@@ -72,17 +72,19 @@ public abstract class EntityMixin {
                     BlockState blockState = world.getBlockState(blockPos);
                     if (!blockState.isAir() && blockState.shouldSuffocate(this.world, blockPos)) {
                         if (suffocationShape == null) {
-                            suffocationShape = VoxelShapes.cuboid(new Box(suffocationMinX, suffocationMinY, suffocationMinZ, suffocationMaxX, suffocationMaxY, suffocationMaxZ));
+                            suffocationShape = VoxelShapes.cuboid(new Box(box.minX, box.minY, box.minZ, box.maxX, box.maxY, box.maxZ));
                         }
                         if (VoxelShapes.matchesAnywhere(blockState.getCollisionShape(this.world, blockPos).
                                         offset(blockPos.getX(), blockPos.getY(), blockPos.getZ()),
                                 suffocationShape, BooleanBiFunction.AND)) {
-                            return true;
+                            cir.setReturnValue(true);
+                            return;
                         }
                     }
                 }
             }
         }
-        return false;
+        cir.setReturnValue(false);
+        return;
     }
 }
