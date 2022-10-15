64078c5544cfdd93907ad0756f0822e0ce761f65
2No2Name <2No2Name@web.de>
2022-10-15 14:44:57 +0200

    change: simplify locked hopper sleeping mixin

--- src/main/java/me/jellysquid/mods/lithium/mixin/world/block_entity_ticking/sleeping/hopper/HopperBlockEntityMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/world/block_entity_ticking/sleeping/hopper/HopperBlockEntityMixin.java
@@ -3,18 +3,17 @@ package me.jellysquid.mods.lithium.mixin.world.block_entity_ticking.sleeping.hop
 import me.jellysquid.mods.lithium.common.block.entity.SleepingBlockEntity;
 import me.jellysquid.mods.lithium.mixin.world.block_entity_ticking.sleeping.WrappedBlockEntityTickInvokerAccessor;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HopperBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.entity.HopperBlockEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.BlockEntityTickInvoker;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -57,23 +56,14 @@ public class HopperBlockEntityMixin extends BlockEntity implements SleepingBlock
         super(type, pos, state);
     }
 
-    @SuppressWarnings("InvalidInjectorMethodSignature" )
-    @ModifyVariable(
-            method = "insertAndExtract",
-            at = @At(value = "JUMP", opcode = Opcodes.IFEQ, ordinal = 2),
-            argsOnly = true
-    )
-    private static BooleanSupplier rememberBranch(BooleanSupplier booleanSupplier) {
-        return null;
-    }
-
     @Inject(
             method = "insertAndExtract",
             at = @At(value = "RETURN", ordinal = 2)
     )
-    private static void sleepIfBranchNotRemembered(World world, BlockPos pos, BlockState state, HopperBlockEntity blockEntity, BooleanSupplier booleanSupplier, CallbackInfoReturnable<Boolean> cir) {
-        if (booleanSupplier != null && !((HopperBlockEntityMixin) (Object) blockEntity).needsCooldown()) {
-            //When this code is reached, rememberBranch(BooleanSupplier) wasn't reached. Therefore the hopper is locked and not on cooldown.
+    private static void sleepIfNoCooldownAndLocked(World world, BlockPos pos, BlockState state, HopperBlockEntity blockEntity, BooleanSupplier booleanSupplier, CallbackInfoReturnable<Boolean> cir) {
+        if (!((HopperBlockEntityMixin) (Object) blockEntity).needsCooldown() &&
+                !((HopperBlockEntityMixin) (Object) blockEntity).isSleeping() &&
+                !state.get(HopperBlock.ENABLED)) {
             ((HopperBlockEntityMixin) (Object) blockEntity).startSleeping();
         }
     }
