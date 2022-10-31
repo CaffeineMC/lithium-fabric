31185aa8ded1ba382604276b0ed10eb00599eafb
2No2Name <2no2name@web.de>
2022-10-31 21:31:43 +0100

    change: switch from old LithiumConfig system to mixin-config annotation system

---
+++ src/main/java/me/jellysquid/mods/lithium/mixin/ai/poi/fast_portals/package-info.java
@@ -0,0 +1,4 @@
+@MixinConfigOption(description = "Portal search uses the faster POI search")
+package me.jellysquid.mods.lithium.mixin.ai.poi.fast_portals;
+
+import net.caffeinemc.gradle.MixinConfigOption;
---
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/suffocation/package-info.java
@@ -0,0 +1,4 @@
+@MixinConfigOption(description = "Avoids stream code in suffocation check")
+package me.jellysquid.mods.lithium.mixin.entity.collisions.suffocation;
+
+import net.caffeinemc.gradle.MixinConfigOption;
