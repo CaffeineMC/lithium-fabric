8fe33438eb4f470dfd8aaa1535956e027f603ef9
2No2Name <2No2Name@web.de>
2022-11-11 15:13:48 +0100

    Backport block entity sleeping

--- src/main/java/me/jellysquid/mods/lithium/mixin/world/block_entity_ticking/sleeping/WorldMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/world/block_entity_ticking/sleeping/WorldMixin.java
@@ -1,6 +1,7 @@
 package me.jellysquid.mods.lithium.mixin.world.block_entity_ticking.sleeping;
 
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,12 +12,23 @@ public class WorldMixin {
 
     @Redirect(
             method = "tickBlockEntities",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;shouldTickBlockPos(Lnet/minecraft/util/math/BlockPos;)Z" )
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;shouldTickBlocksInChunk(J)Z" )
     )
-    private boolean shouldTickBlockPosFilterNull(World instance, BlockPos pos) {
-        if (pos == null) {
+    private boolean shouldTickBlockPosFilterNull(World instance, long chunkPos) {
+        if (chunkPos == Long.MIN_VALUE) {
             return false;
         }
-        return instance.shouldTickBlockPos(pos);
+        return instance.shouldTickBlocksInChunk(chunkPos);
+    }
+
+    @Redirect(
+            method = "tickBlockEntities",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/ChunkPos;toLong(Lnet/minecraft/util/math/BlockPos;)J" )
+    )
+    private long shouldTickBlockPosFilterNull(BlockPos pos) {
+        if (pos == null) {
+            return Long.MIN_VALUE;
+        }
+        return ChunkPos.toLong(pos);
     }
 }
