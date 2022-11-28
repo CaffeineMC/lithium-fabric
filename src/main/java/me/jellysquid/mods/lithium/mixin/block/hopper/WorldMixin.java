b73cd46572d00decb483b08eb8593cab4ae217a3
2No2Name <2No2Name@web.de>
2022-11-28 03:48:11 +0100

    fix: build fail due to import issue from previous commit

--- src/main/java/me/jellysquid/mods/lithium/mixin/block/hopper/WorldMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/block/hopper/WorldMixin.java
@@ -10,7 +10,9 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.WorldChunk;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -18,10 +20,12 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Map;
 
-import static net.minecraft.client.render.WorldRenderer.DIRECTIONS;
-
 @Mixin(World.class)
 public class WorldMixin {
+
+    @Shadow
+    @Final
+    private static Direction[] DIRECTIONS;
 
     @Inject(
             method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;II)Z",
