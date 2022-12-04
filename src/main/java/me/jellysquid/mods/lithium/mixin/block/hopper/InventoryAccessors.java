95744030e79d43fcb866fec12917e0ef3e8712cb
2No2Name <2No2Name@web.de>
2022-12-04 23:05:15 +0100

    Revert "change: block.hopper: simplify inventory accessor mixin"
    This reverts commit 9f3e85b8

--- src/main/java/me/jellysquid/mods/lithium/mixin/block/hopper/InventoryAccessors.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/block/hopper/InventoryAccessors.java
@@ -1,53 +1,86 @@
 package me.jellysquid.mods.lithium.mixin.block.hopper;
 
 import me.jellysquid.mods.lithium.api.inventory.LithiumInventory;
-import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
-import net.minecraft.block.entity.BrewingStandBlockEntity;
-import net.minecraft.block.entity.LootableContainerBlockEntity;
+import net.minecraft.block.entity.*;
 import net.minecraft.entity.vehicle.StorageMinecartEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 public class InventoryAccessors {
-    @Mixin(LootableContainerBlockEntity.class)
-    public abstract static class InventoryAccessorLootableContainerBlockEntity implements LithiumInventory {
-        @Shadow
-        protected abstract DefaultedList<ItemStack> getInvStackList();
-
-        @Shadow
-        protected abstract void setInvStackList(DefaultedList<ItemStack> list);
-
-        @Override
-        public DefaultedList<ItemStack> getInventoryLithium() {
-            return this.getInvStackList();
-        }
-
-        @Override
-        public void setInventoryLithium(DefaultedList<ItemStack> inventory) {
-            this.setInvStackList(inventory);
-        }
-    }
-
     @Mixin(AbstractFurnaceBlockEntity.class)
     public abstract static class InventoryAccessorAbstractFurnaceBlockEntity implements LithiumInventory {
-        @Accessor("inventory" )
+        @Accessor("inventory")
         public abstract DefaultedList<ItemStack> getInventoryLithium();
 
-        @Accessor("inventory" )
+        @Accessor("inventory")
+        public abstract void setInventoryLithium(DefaultedList<ItemStack> inventory);
+    }
+
+    @Mixin(BarrelBlockEntity.class)
+    public abstract static class InventoryAccessorBarrelBlockEntity implements LithiumInventory {
+        @Override
+        @Accessor("inventory")
+        public abstract DefaultedList<ItemStack> getInventoryLithium();
+
+        @Override
+        @Accessor("inventory")
         public abstract void setInventoryLithium(DefaultedList<ItemStack> inventory);
     }
 
     @Mixin(BrewingStandBlockEntity.class)
     public abstract static class InventoryAccessorBrewingStandBlockEntity implements LithiumInventory {
         @Override
-        @Accessor("inventory" )
+        @Accessor("inventory")
         public abstract DefaultedList<ItemStack> getInventoryLithium();
 
         @Override
-        @Accessor("inventory" )
+        @Accessor("inventory")
+        public abstract void setInventoryLithium(DefaultedList<ItemStack> inventory);
+    }
+
+    @Mixin(ChestBlockEntity.class)
+    public abstract static class InventoryAccessorChestBlockEntity implements LithiumInventory {
+        @Override
+        @Accessor("inventory")
+        public abstract DefaultedList<ItemStack> getInventoryLithium();
+
+        @Override
+        @Accessor("inventory")
+        public abstract void setInventoryLithium(DefaultedList<ItemStack> inventory);
+    }
+
+    @Mixin(DispenserBlockEntity.class)
+    public abstract static class InventoryAccessorDispenserBlockEntity implements LithiumInventory {
+        @Override
+        @Accessor("inventory")
+        public abstract DefaultedList<ItemStack> getInventoryLithium();
+
+        @Override
+        @Accessor("inventory")
+        public abstract void setInventoryLithium(DefaultedList<ItemStack> inventory);
+    }
+
+    @Mixin(HopperBlockEntity.class)
+    public abstract static class InventoryAccessorHopperBlockEntity implements LithiumInventory {
+        @Override
+        @Accessor("inventory")
+        public abstract DefaultedList<ItemStack> getInventoryLithium();
+
+        @Override
+        @Accessor("inventory")
+        public abstract void setInventoryLithium(DefaultedList<ItemStack> inventory);
+    }
+
+    @Mixin(ShulkerBoxBlockEntity.class)
+    public abstract static class InventoryAccessorShulkerBoxBlockEntity implements LithiumInventory {
+        @Override
+        @Accessor("inventory")
+        public abstract DefaultedList<ItemStack> getInventoryLithium();
+
+        @Override
+        @Accessor("inventory")
         public abstract void setInventoryLithium(DefaultedList<ItemStack> inventory);
     }
 
