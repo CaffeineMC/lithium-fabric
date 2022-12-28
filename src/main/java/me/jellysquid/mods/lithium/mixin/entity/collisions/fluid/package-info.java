557980e7e70e8031d202dfbc3c52973f23d20490
2No2Name <2No2Name@web.de>
2022-12-28 11:06:52 +0100

    fix: add missing mixin config dependency

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/fluid/package-info.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/fluid/package-info.java
@@ -1,4 +1,8 @@
-@MixinConfigOption(description = "Skips being pushed by fluids when the nearby chunk sections do not contain this fluid")
+@MixinConfigOption(
+        description = "Skips being pushed by fluids when the nearby chunk sections do not contain this fluid",
+        depends = @MixinConfigDependency(dependencyPath = "mixin.util.block_tracking")
+)
 package me.jellysquid.mods.lithium.mixin.entity.collisions.fluid;
 
+import net.caffeinemc.gradle.MixinConfigDependency;
 import net.caffeinemc.gradle.MixinConfigOption;
