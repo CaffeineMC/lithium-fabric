9d50f7db1bcdaa6128fd2da614074a7a81e76e12
2No2Name <2No2Name@web.de>
2023-03-19 11:39:58 +0100

    change: split campfire sleeping into packages for lit and unlit sleeping

--- src/main/java/me/jellysquid/mods/lithium/mixin/world/block_entity_ticking/sleeping/campfire/CampfireBlockEntityMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/world/block_entity_ticking/sleeping/campfire/CampfireBlockEntityMixin.java
@@ -10,14 +10,12 @@ import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraft.world.chunk.BlockEntityTickInvoker;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(CampfireBlockEntity.class)
 public class CampfireBlockEntityMixin extends BlockEntity implements SleepingBlockEntity {
@@ -65,29 +63,5 @@ public class CampfireBlockEntityMixin extends BlockEntity implements SleepingBlo
     )
     private void wakeUpOnReadNbt(NbtCompound nbt, CallbackInfo ci) {
         this.wakeUpNow();
-    }
-
-    @Inject(
-            method = "unlitServerTick",
-            at = @At("RETURN"),
-            locals = LocalCapture.CAPTURE_FAILHARD
-    )
-    private static void trySleepUnlit(World world, BlockPos pos, BlockState state, CampfireBlockEntity campfire, CallbackInfo ci, boolean hadProgress) {
-        if (!hadProgress) {
-            CampfireBlockEntityMixin self = (CampfireBlockEntityMixin) (Object) campfire;
-            self.startSleeping();
-        }
-    }
-
-    @Inject(
-            method = "litServerTick",
-            at = @At("RETURN" ),
-            locals = LocalCapture.CAPTURE_FAILHARD
-    )
-    private static void trySleepLit(World world, BlockPos pos, BlockState state, CampfireBlockEntity campfire, CallbackInfo ci, boolean hadProgress) {
-        if (!hadProgress) {
-            CampfireBlockEntityMixin self = (CampfireBlockEntityMixin) (Object) campfire;
-            self.startSleeping();
-        }
     }
 }
