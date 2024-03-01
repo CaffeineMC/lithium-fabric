3a32d720e954364ecb218c02d7cefe191b99223a
2No2Name <2no2name@web.de>
2024-03-01 18:12:05 +0100

    Check Beehive obstruction first on exit

---
+++ src/main/java/me/jellysquid/mods/lithium/mixin/block/beehive/package-info.java
@@ -0,0 +1,4 @@
+@MixinConfigOption(description = "Beehives check if they are obstructed before performing expensive nbt copy operations for each exit attempt.")
+package me.jellysquid.mods.lithium.mixin.block.beehive;
+
+import net.caffeinemc.gradle.MixinConfigOption;
