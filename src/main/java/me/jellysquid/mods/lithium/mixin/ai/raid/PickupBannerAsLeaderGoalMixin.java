b2a99e729d4cb287f643251e03779f506ddcb2ce
Drex <nicknamedrex@gmail.com>
2024-03-15 19:25:52 +0100

    Fix ominous banner caching

--- src/main/java/me/jellysquid/mods/lithium/mixin/ai/raid/PickupBannerAsLeaderGoalMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/ai/raid/PickupBannerAsLeaderGoalMixin.java
@@ -1,29 +1,30 @@
 package me.jellysquid.mods.lithium.mixin.ai.raid;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.block.entity.BannerPattern;
 import net.minecraft.entity.raid.RaiderEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.BuiltinRegistries;
 import net.minecraft.registry.RegistryEntryLookup;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.village.raid.Raid;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RaiderEntity.PickupBannerAsLeaderGoal.class)
 public class PickupBannerAsLeaderGoalMixin {
     // The call to Raid#getOminousBanner() is very expensive, so cache it and re-use it during AI ticking
-    private static final ItemStack CACHED_OMINOUS_BANNER = Raid.getOminousBanner(BuiltinRegistries.createWrapperLookup().getWrapperOrThrow(RegistryKeys.BANNER_PATTERN));
+    private static ItemStack CACHED_OMINOUS_BANNER;
 
-    @Redirect(
+    @WrapOperation(
             method = "canStart()Z",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/village/raid/Raid;getOminousBanner(Lnet/minecraft/registry/RegistryEntryLookup;)Lnet/minecraft/item/ItemStack;"
             )
     )
-    private ItemStack getOminousBanner(RegistryEntryLookup<BannerPattern> registryEntryLookup) {
+    private ItemStack getOminousBanner(RegistryEntryLookup<BannerPattern> bannerPatternLookup, Operation<ItemStack> original) {
+        if (CACHED_OMINOUS_BANNER == null) {
+            CACHED_OMINOUS_BANNER = original.call(bannerPatternLookup);
+        }
         return CACHED_OMINOUS_BANNER;
     }
 }
