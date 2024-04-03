d9fd6025cdfbfa350b69e13d7593bf7281011b47
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2024-04-03 00:20:32 -0400

    dont pre-emptively calculate exposure

--- src/main/java/me/jellysquid/mods/lithium/common/world/ExplosionCache.java
+++ src/main/java/me/jellysquid/mods/lithium/common/world/ExplosionCache.java
@@ -3,6 +3,5 @@ package me.jellysquid.mods.lithium.common.world;
 import net.minecraft.entity.Entity;
 
 public interface ExplosionCache {
-    float lithium_fabric$getCachedExposure();
-    Entity lithium_fabric$getCachedEntity();
+    void lithium_fabric$cacheExposure(Entity entity, float exposure);
 }
