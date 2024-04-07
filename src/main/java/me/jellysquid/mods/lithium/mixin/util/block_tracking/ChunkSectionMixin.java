7799e2a1d463bd880887c7067ea1e7aa190b2796
Steveplays28 <djspaargaren@outlook.com>
2024-04-07 14:37:05 +0200

    feat: Add `lithium$trackBlockStateChange` method to `BlockCountingSection`
    This allows other mods such as Noisium and Big Globe to call this function using the `BlockCountingSection` interface to track blockstates (increment/decrement the blockstate counter) during world generation, for example.

--- src/main/java/me/jellysquid/mods/lithium/mixin/util/block_tracking/ChunkSectionMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/util/block_tracking/ChunkSectionMixin.java
@@ -30,6 +30,7 @@ public abstract class ChunkSectionMixin implements BlockCountingSection, BlockLi
     @Shadow
     @Final
     private PalettedContainer<BlockState> blockStateContainer;
+
     @Unique
     private short[] countsByFlag = null;
     private ChunkSectionChangeCallback changeListener;
@@ -102,6 +103,11 @@ public abstract class ChunkSectionMixin implements BlockCountingSection, BlockLi
             locals = LocalCapture.CAPTURE_FAILHARD
     )
     private void updateFlagCounters(int x, int y, int z, BlockState newState, boolean lock, CallbackInfoReturnable<BlockState> cir, BlockState oldState) {
+        this.lithium$trackBlockStateChange(newState, oldState);
+    }
+
+    @Override
+    public void lithium$trackBlockStateChange(BlockState newState, BlockState oldState) {
         short[] countsByFlag = this.countsByFlag;
         if (countsByFlag == null) {
             return;
