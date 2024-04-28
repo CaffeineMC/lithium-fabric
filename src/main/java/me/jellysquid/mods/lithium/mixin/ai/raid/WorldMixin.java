093f50bd28deb0e22eb7dd5485137ccb5e77320b
2No2Name <2no2name@web.de>
2024-04-28 13:58:12 +0200

    Fix ominous banner caching mod incompatibility
    Previous issue with litematica's schematic worlds

--- src/main/java/me/jellysquid/mods/lithium/mixin/ai/raid/WorldMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/ai/raid/WorldMixin.java
@@ -10,7 +10,7 @@ import net.minecraft.village.raid.Raid;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,18 +19,17 @@ import java.util.function.Supplier;
 
 @Mixin(World.class)
 public abstract class WorldMixin implements OminousBannerCache {
-    @Shadow
-    public abstract DynamicRegistryManager getRegistryManager();
 
     // The call to Raid#getOminousBanner() is very expensive, so cache it and re-use it during AI ticking
+    @Unique
     private ItemStack cachedOminousBanner;
 
     @Inject(
             method = "<init>",
             at = @At("RETURN")
     )
-    protected void initCachedOminiousBanner(MutableWorldProperties properties, RegistryKey<?> registryRef, DynamicRegistryManager registryManager, RegistryEntry<?> dimensionEntry, Supplier<?> profiler, boolean isClient, boolean debugWorld, long biomeAccess, int maxChainedNeighborUpdates, CallbackInfo ci) {
-        this.cachedOminousBanner = Raid.getOminousBanner(this.getRegistryManager().getWrapperOrThrow(RegistryKeys.BANNER_PATTERN));
+    protected void initCachedOminousBanner(MutableWorldProperties properties, RegistryKey<?> registryRef, DynamicRegistryManager registryManager, RegistryEntry<?> dimensionEntry, Supplier<?> profiler, boolean isClient, boolean debugWorld, long biomeAccess, int maxChainedNeighborUpdates, CallbackInfo ci) {
+        this.cachedOminousBanner = Raid.getOminousBanner(registryManager.getWrapperOrThrow(RegistryKeys.BANNER_PATTERN));
     }
 
     @Override
