ffae0736721440c4d8389b931e2671f1e6e318a0
2No2Name <2no2name@web.de>
2024-05-08 01:09:41 +0200

    Deprecate previous item listening system

--- src/main/java/me/jellysquid/mods/lithium/common/entity/item/ItemStackSubscriber.java
+++ src/main/java/me/jellysquid/mods/lithium/common/entity/item/ItemStackSubscriber.java
@@ -3,6 +3,7 @@ package me.jellysquid.mods.lithium.common.entity.item;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.ItemStack;
 
+@Deprecated
 public interface ItemStackSubscriber {
 
     void lithium$notifyBeforeCountChange(ItemStack itemStack, int index, int newCount);
--- src/main/java/me/jellysquid/mods/lithium/common/hopper/NotifyingItemStack.java
+++ src/main/java/me/jellysquid/mods/lithium/common/hopper/NotifyingItemStack.java
@@ -4,6 +4,7 @@ import me.jellysquid.mods.lithium.common.entity.item.ItemStackSubscriber;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.ItemStack;
 
+@Deprecated
 public interface NotifyingItemStack {
     @SuppressWarnings("unused")
     void lithium$subscribe(ItemStackSubscriber subscriber);
