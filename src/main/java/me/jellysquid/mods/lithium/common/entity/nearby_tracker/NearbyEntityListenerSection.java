82f0e8c711c83218b8b3e1d7ec04f61c24ad68aa
2No2Name <2no2name@web.de>
2024-05-09 03:05:52 +0200

    Remove leftovers from NearbyEntityListener system

--- src/main/java/me/jellysquid/mods/lithium/common/entity/nearby_tracker/NearbyEntityListenerSection.java
+++
@@ -1,9 +0,0 @@
-package me.jellysquid.mods.lithium.common.entity.nearby_tracker;
-
-import net.minecraft.world.entity.SectionedEntityCache;
-
-public interface NearbyEntityListenerSection {
-    void lithium$addListener(NearbyEntityListener listener);
-
-    void lithium$removeListener(SectionedEntityCache<?> sectionedEntityCache, NearbyEntityListener listener);
-}
