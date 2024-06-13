a262bc594f788f6dd95b883e6f7720494ef4b198
2No2Name <2no2name@web.de>
2024-06-13 20:42:36 +0200

    fix: pathfinding returning OPEN due to 1.20-pre5 update issue

--- src/main/java/me/jellysquid/mods/lithium/mixin/util/block_tracking/AbstractBlockStateMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/util/block_tracking/AbstractBlockStateMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AbstractBlock.AbstractBlockState.class)
+@Mixin(value = AbstractBlock.AbstractBlockState.class, priority = 1010)
 public class AbstractBlockStateMixin implements BlockStateFlagHolder {
     @Unique
     private int flags;
