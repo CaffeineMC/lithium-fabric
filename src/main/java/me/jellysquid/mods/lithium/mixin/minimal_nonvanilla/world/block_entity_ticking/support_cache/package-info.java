05b5264a8a5bd1497522859b3820b75cfddc60af
2No2Name <2no2name@web.de>
2024-06-15 22:40:01 +0200

    Disable new optimizations with mod compat issues before release

--- src/main/java/me/jellysquid/mods/lithium/mixin/minimal_nonvanilla/world/block_entity_ticking/support_cache/package-info.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/minimal_nonvanilla/world/block_entity_ticking/support_cache/package-info.java
@@ -3,7 +3,8 @@
                 " This deviates from vanilla in the case of placing a hopper in a powered location, immediately updating the" +
                 " cached BlockState (which is incorrect in vanilla). This most likely does not affect your gameplay, as this" +
                 " deviation only affects hoppers, and in vanilla, hoppers never use the cached state information anyway.",
-        depends = @MixinConfigDependency(dependencyPath = "mixin.world.block_entity_ticking")
+        depends = @MixinConfigDependency(dependencyPath = "mixin.world.block_entity_ticking"),
+        enabled = false //Lots of mod incompatibilities, TODO: analysis -> change the mixins or suggest changes to carpet & world edit
 )
 package me.jellysquid.mods.lithium.mixin.minimal_nonvanilla.world.block_entity_ticking.support_cache;
 
--- src/main/java/me/jellysquid/mods/lithium/mixin/world/explosions/cache_exposure/package-info.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/world/explosions/cache_exposure/package-info.java
@@ -1,4 +1,7 @@
-@MixinConfigOption(description = "Caches entity explosion exposure to avoid duplicate calculations.")
+@MixinConfigOption(
+        description = "Caches entity explosion exposure to avoid duplicate calculations.",
+        enabled = false // TODO mod incompatibility with carpet
+)
 package me.jellysquid.mods.lithium.mixin.world.explosions.cache_exposure;
 
 import net.caffeinemc.gradle.MixinConfigOption;
