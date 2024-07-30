409a5e12f50918a911946093ea96aa60bdf6b901
2No2Name <2no2name@web.de>
2024-07-30 22:14:25 +0200

    fix: Crash when hoppers interact with any Inventory that is not a Chiseled Bookshelf

--- src/main/java/me/jellysquid/mods/lithium/mixin/block/hopper/InventoryMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/block/hopper/InventoryMixin.java
@@ -1,9 +1,10 @@
 package me.jellysquid.mods.lithium.mixin.block.hopper;
 
 import me.jellysquid.mods.lithium.api.inventory.LithiumCooldownReceivingInventory;
+import me.jellysquid.mods.lithium.api.inventory.LithiumTransferConditionInventory;
 import net.minecraft.inventory.Inventory;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(Inventory.class)
-public interface InventoryMixin extends LithiumCooldownReceivingInventory {
+public interface InventoryMixin extends LithiumCooldownReceivingInventory, LithiumTransferConditionInventory {
 }
