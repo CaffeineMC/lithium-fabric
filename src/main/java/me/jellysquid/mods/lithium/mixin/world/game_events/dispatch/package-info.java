be3e77c5c16ca4de3117c0d77de023a3c2005a03
2No2Name <2no2name@web.de>
2024-08-24 19:03:55 +0200

    Disable broken mixin.world.game_events.dispatch optimization by default

--- src/main/java/me/jellysquid/mods/lithium/mixin/world/game_events/dispatch/package-info.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/world/game_events/dispatch/package-info.java
@@ -3,7 +3,8 @@
         " listener is added to a section. This reduces memory usage for chunks that do not have any listeners." +
         " The dispatchers are accessed more directly instead of indirectly through chunks." +
         " In total this speeds up attempting to dispatch events especially when there are no nearby listeners.",
-        depends = @MixinConfigDependency(dependencyPath = "mixin.util.data_storage")
+        depends = @MixinConfigDependency(dependencyPath = "mixin.util.data_storage"),
+        enabled = false
 )
 package me.jellysquid.mods.lithium.mixin.world.game_events.dispatch;
 
