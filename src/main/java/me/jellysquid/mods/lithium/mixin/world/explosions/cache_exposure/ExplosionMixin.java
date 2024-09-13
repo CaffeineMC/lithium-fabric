dc9568ce62a9d8dc8f3b90ff752050d9b094e73c
wendavid552 <david.jianwei.wen@gmail.com>
2024-09-14 00:47:18 +0800

    Fix incompatability with scarpet explosion event.

--- src/main/java/me/jellysquid/mods/lithium/mixin/world/explosions/cache_exposure/ExplosionMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/world/explosions/cache_exposure/ExplosionMixin.java
@@ -1,5 +1,7 @@
 package me.jellysquid.mods.lithium.mixin.world.explosions.cache_exposure;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import me.jellysquid.mods.lithium.common.world.ExplosionCache;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Vec3d;
@@ -7,7 +9,6 @@ import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * Optimizations for Explosions: Remove duplicate {@link Explosion#getExposure(Vec3d, Entity)} calls.
@@ -24,8 +25,8 @@ public abstract class ExplosionMixin implements ExplosionCache {
         this.cachedEntity = entity;
     }
 
-    @Redirect(method = "collectBlocksAndDamageEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/explosion/Explosion;getExposure(Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/entity/Entity;)F"))
-    private float returnCachedExposure(Vec3d source, Entity entity) {
-        return this.cachedEntity == entity ? this.cachedExposure : Explosion.getExposure(source, entity);
+    @WrapOperation(method = "collectBlocksAndDamageEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/explosion/Explosion;getExposure(Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/entity/Entity;)F"))
+    private float returnCachedExposure(Vec3d source, Entity entity, Operation<Float> original) {
+        return this.cachedEntity == entity ? this.cachedExposure : original.call(source, entity);
     }
 }
