c2b1cfe81ddd2228d9f317f583db82f157d7ef41
2No2Name <2No2Name@web.de>
2026-09-15 23:45:11 +0200

    Add fabric stuff back to common source set for IDE usability

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,6 +2,48 @@
 plugins {
     id("java")
     id("idea")
+    id("net.fabricmc.fabric-loom") version ("1.17-SNAPSHOT")
+}
+
+val MINECRAFT_COMPILE_VERSION: String by rootProject.extra
+val FABRIC_LOADER_VERSION: String by rootProject.extra
+val FABRIC_API_VERSION: String by rootProject.extra
+
+dependencies {
+    minecraft(group = "com.mojang", name = "minecraft", version = MINECRAFT_COMPILE_VERSION)
+    compileOnly("net.fabricmc:fabric-loader:$FABRIC_LOADER_VERSION")
+
+    fun addDependentFabricModule(name: String) {
+        val module = fabricApi.module(name, FABRIC_API_VERSION)
+        compileOnly(module)
+    }
+
+    fun addEmbeddedFabricModule(name: String) {
+        val module = fabricApi.module(name, FABRIC_API_VERSION)
+        implementation(module)
+        include(module)
+    }
+
+    fun addCompileOnlyFabricModule(name: String) {
+        val module = fabricApi.module(name, FABRIC_API_VERSION)
+        compileOnly(module)
+    }
+
+    fun addFabricModule(name: String) {
+        val module = fabricApi.module(name, FABRIC_API_VERSION)
+        implementation(module)
+    }
+
+    //Copied from fabric build.gradle to avoid having multiple slightly different (few fabric interfaces added) minecraft merged mapped jars
+    addCompileOnlyFabricModule("fabric-transfer-api-v1")
+    addFabricModule("fabric-gametest-api-v1")
+    addFabricModule("fabric-registry-sync-v0")
+
+
+    // example usage:
+    //    addDependentFabricModule("fabric-block-view-api-v2")
+
+    compileOnly("net.caffeinemc:mixin-config-plugin:1.0-SNAPSHOT")
 }
 
 sourceSets {
@@ -53,6 +95,20 @@ tasks.named("build") {
     dependsOn("apiJar")
 }
 
+loom {
+    mixin {
+        useLegacyMixinAp = false
+    }
+
+    accessWidenerPath = file("src/main/resources/lithium.accesswidener")
+
+    mods {
+        val main by creating { // to match the default mod generated for Forge
+            sourceSet("api")
+            sourceSet("main")
+        }
+    }
+}
 
 tasks {
     jar {
