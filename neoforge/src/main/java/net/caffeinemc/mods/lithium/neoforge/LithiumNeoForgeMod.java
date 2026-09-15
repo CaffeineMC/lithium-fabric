845fcf8d547de1f1b2bed44e678c62db79896598
2No2Name <2No2Name@web.de>
2026-09-15 23:01:26 +0200

    Fix Event Bus registration

--- neoforge/src/main/java/net/caffeinemc/mods/lithium/neoforge/LithiumNeoForgeMod.java
+++ neoforge/src/main/java/net/caffeinemc/mods/lithium/neoforge/LithiumNeoForgeMod.java
@@ -4,11 +4,12 @@ import net.caffeinemc.mods.lithium.common.initialization.BlockInfoInitializer;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.registries.datamaps.DataMapsUpdatedEvent;
 
 @Mod(value = "lithium")
 public class LithiumNeoForgeMod {
     public LithiumNeoForgeMod(IEventBus bus, ModContainer modContainer) {
-        bus.addListener(DataMapsUpdatedEvent.class, event -> BlockInfoInitializer.resetBlockInfo());
+        NeoForge.EVENT_BUS.addListener(DataMapsUpdatedEvent.class, event -> BlockInfoInitializer.resetBlockInfo());
     }
 }
